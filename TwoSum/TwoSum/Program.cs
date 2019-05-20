018b1053dbbfb24c192d5317d7bb9f518795177f
Timur Koshel <t.koshel@come2play.com>
2019-05-20 15:33:47 +0300

    TwoSum solved

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    var num1 = nums[i];
+                    var num2 = nums[j];
+
+                    if (num1 + num2 == target)
+                    {
+                        return new int[] {i , j};
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
