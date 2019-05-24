d8bb6a0e3b6b81b409d032373f040206fd10a098
Timur Koshel <t.koshel@come2play.com>
2019-05-24 09:43:20 +0300

    Search Insert Position

---
+++ SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SearchInsertPosition
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().SearchInsert(new []{ 1, 3, 5, 6}, 5));
+            Console.WriteLine(1 == new Solution().SearchInsert(new []{ 1, 3, 5, 6}, 2));
+            Console.WriteLine(4 == new Solution().SearchInsert(new []{ 1, 3, 5, 6}, 7));
+            Console.WriteLine(0 == new Solution().SearchInsert(new []{ 1, 3, 5, 6}, 0));
+        }
+    }
+}
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target)
+    {
+        var l = 0;
+        var r = nums.Length;
+
+        while (r > l)
+        {
+            var middle = (l + r) / 2;
+
+            if (target > nums[middle])
+            {
+                l = middle + 1;
+            }
+            else if (target < nums[middle])
+            {
+                r = middle;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+        return l;
+    }
+}
