488045dbb52c1a544543c7977940de96da83bd71
Timur Koshel <t.koshel@come2play.com>
2019-05-27 09:49:38 +0300

    Count and say, maximum subarray

---
+++ CountAndSay/CountAndSay/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CountAndSay
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1" == new Solution().CountAndSay(1));
+            Console.WriteLine("1211" == new Solution().CountAndSay(4));
+        }
+    }
+}
+
+public class Solution {
+    public string CountAndSay(int n)
+    {
+        var result = "1";
+
+        for (var i = 1; i < n; i++)
+        {
+            var tmp = string.Empty;
+            var count = 1;
+
+            for (var j = 1; j < result.Length; j++)
+            {
+                if (result[j - 1] == result[j])
+                {
+                    count += 1;
+                }
+                else
+                {
+                    tmp += count.ToString() + result[j - 1];
+                    count = 1;
+                }
+            }
+
+            result = tmp + count.ToString() + result[result.Length - 1];
+        }
+
+        return result;
+    }
+}
