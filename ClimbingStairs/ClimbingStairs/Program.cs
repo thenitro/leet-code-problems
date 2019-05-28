5ba6b40207ddc292c05d945067eee1fae558a31a
Timur Koshel <t.koshel@come2play.com>
2019-05-28 11:42:23 +0300

    ClimbingStairs

---
+++ ClimbingStairs/ClimbingStairs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClimbingStairs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().ClimbStairs(2));
+            Console.WriteLine(3 == new Solution().ClimbStairs(3));
+        }
+    }
+}
+
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        var first = 1;
+        var second = 2;
+
+        for (var i = 3; i <= n; i++)
+        {
+            var third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+}
