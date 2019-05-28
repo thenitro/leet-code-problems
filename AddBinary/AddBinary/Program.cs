ecb47c4535313b123f30660a88fe684566f30dbd
Timur Koshel <t.koshel@come2play.com>
2019-05-28 08:21:46 +0300

    AddBinary, PlusOne

---
+++ AddBinary/AddBinary/Program.cs
@@ -0,0 +1,84 @@
+﻿﻿using System;
+
+namespace AddBinary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("100" == new Solution().AddBinary("11", "1"));
+            Console.WriteLine("10101" == new Solution().AddBinary("1010", "1011"));
+            Console.WriteLine("1010" == new Solution().AddBinary("111", "11"));
+        }
+    }
+}
+
+public class Solution {
+    public string AddBinary(string a, string b)
+    {
+        var result = string.Empty;
+        var addOne = false;
+        
+        while (!string.IsNullOrEmpty(a) || !string.IsNullOrEmpty(b))
+        {
+            var preResult = '0';
+            var addOneNow = addOne;
+
+            addOne = false;
+            
+            var charA = a.Length > 0 ? a[a.Length - 1] : '0';
+            if (a.Length > 0)
+            {
+                a = a.Remove(a.Length - 1);                
+            }
+            var charB = b.Length > 0 ? b[b.Length - 1] : '0';
+            if (b.Length > 0)
+            {
+                b = b.Remove(b.Length - 1);                
+            }
+            
+            //Console.WriteLine(charA + " " + charB);
+            
+            if (charA == '1' && charB == '1')
+            {
+                addOne = true;
+            } 
+            else if (charA == '0' && charB == '0')
+            {
+            }
+            else
+            {
+                preResult = '1';
+            }
+
+            //Console.WriteLine(addOne + " " + addOneNow);
+            
+            if (addOneNow)
+            {
+                if (preResult == '0')
+                {
+                    preResult = '1';
+                }
+                else
+                {
+                    preResult = '0';
+                    addOne = true;
+                }
+            }
+            
+            result = preResult + result;
+            
+            //Console.WriteLine(preResult);
+            //Console.WriteLine("------");
+        }
+
+        if (addOne)
+        {
+            result = '1' + result;
+        }
+        
+        //Console.WriteLine(result);
+
+        return result;
+    }
+}
---
+++ PlusOne/PlusOne/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlusOne
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintArray(new Solution().PlusOne(new int[] { 1, 2, 3 }));
+            PrintArray(new Solution().PlusOne(new int[] { 4, 3, 2, 1 }));
+            PrintArray(new Solution().PlusOne(new int[] { 1 }));
+            PrintArray(new Solution().PlusOne(new int[] { 9 }));
+            PrintArray(new Solution().PlusOne(new int[] { 9, 9 }));
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            
+            Console.WriteLine();
+        }
+        
+    }
+}
+
+public class Solution {
+    public int[] PlusOne(int[] digits)
+    {
+        var length = digits.Length;
+        
+        var list = new Stack<int>();
+
+        var addOne = false;
+        var lastElement = digits[length - 1];
+            lastElement++;
+
+        if (lastElement > 9)
+        {
+            lastElement -= 10;
+            addOne = true;
+        }
+        
+        list.Push(lastElement);
+
+        for (var i = length - 2; i >= 0; i--)
+        {
+            var n = digits[i];
+            
+            if (addOne)
+            {
+                n++;
+                addOne = false;
+            }
+
+            if (n > 9)
+            {
+                n -= 10;
+                addOne = true;
+            }
+            
+            list.Push(n);
+        }
+
+        if (addOne)
+        {
+            list.Push(1);
+        }
+
+        return list.ToArray();
+    }
+}
