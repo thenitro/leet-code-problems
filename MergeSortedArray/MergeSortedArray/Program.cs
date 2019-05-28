68ebec9c42a06a7228bb1f3fc7bf77180f0197a8
Timur Koshel <t.koshel@come2play.com>
2019-05-28 15:57:04 +0300

    merge sorted array, remove duplicates from sorted list

---
+++ MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.Remoting.Lifetime;
+
+namespace MergeSortedArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var num1 = new int[] {1, 2, 3, 0, 0, 0};
+            
+            new Solution().Merge(num1, 3, new int[] { 2, 5, 6 }, 3);
+            
+            foreach (var i in num1)
+            {
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
+
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        for (var i = 0; i < n; i++)
+        {
+            var element2 = nums2[i];
+            var putIndex = FindPutIndex(nums1, m, element2);
+
+            for (var k = nums1.Length - 1; k > putIndex; k--)
+            {
+                nums1[k] = nums1[k - 1];
+            }
+
+            nums1[putIndex] = element2;
+            
+            m++;
+        }
+    }
+
+    private int FindPutIndex(int[] nums, int size, int element)
+    {
+        var l = 0;
+        var r = size;
+
+        while (l < r)
+        {
+            var middle = (l + r) / 2;
+            
+            if (element > nums[middle])
+            {
+                l = middle + 1;
+            }
+            else
+            {
+                r = middle;
+            }
+        }
+
+        return l;
+    }
+}
---
+++ RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace RemoveDuplicatesFromSortedList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintList(new Solution().DeleteDuplicates(null));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1) { next = new ListNode(1)}));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1) { next = new ListNode(1) { next = new ListNode(1) }}));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1) ));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1){ next = new ListNode(2) }));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1) { next = new ListNode(1){ next = new ListNode(2) }}));
+            PrintList(new Solution().DeleteDuplicates(new ListNode(1) { next = new ListNode(1){ next = new ListNode(2) { next = new ListNode(3) { next = new ListNode(3) }} }}));
+        }
+
+        private static void PrintList(ListNode head)
+        {
+            Console.WriteLine();
+
+            var next = head;
+            
+            while (next != null)
+            {
+                Console.Write(next.val + " ");
+                next = next.next;
+            }
+        }
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        
+        var prev = head;
+        var current = prev.next;
+
+        while (current != null)
+        {
+            if (current.val == prev.val)
+            {
+                prev.next = current.next;
+            }
+            else
+            {
+                prev = current;
+            }
+            
+            current = current.next;
+        }
+
+        return head;
+    }
+}
