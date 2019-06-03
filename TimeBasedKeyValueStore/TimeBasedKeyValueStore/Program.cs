9593b0489a5ebefee6fc5f8fc77da8b8f9c0f773
Timur Koshel <timurkoshel@gmail.com>
2019-06-03 15:14:16 +0300

    Time based key value store and vertical order traversal of a binary tree

---
+++ TimeBasedKeyValueStore/TimeBasedKeyValueStore/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeBasedKeyValueStore
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var kv = new TimeMap();
+            
+            kv.Set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1   
+            Console.WriteLine(kv.Get("foo", 1));  // output "bar"   
+            Console.WriteLine(kv.Get("foo", 3)); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"   
+            kv.Set("foo", "bar2", 4);   
+            Console.WriteLine(kv.Get("foo", 4)); // output "bar2"   
+            Console.WriteLine(kv.Get("foo", 5)); //output "bar2"   
+            
+            var kv2 = new TimeMap();
+                kv2.Set("love", "high", 10);
+                kv2.Set("love", "low", 20);
+                    
+            Console.WriteLine(kv2.Get("love", 5));//""
+            Console.WriteLine(kv2.Get("love", 10));//"high"
+            Console.WriteLine(kv2.Get("love", 15));//"high"
+            Console.WriteLine(kv2.Get("love", 20));//"low"
+            Console.WriteLine(kv2.Get("love", 25));//"low"
+        }
+    }
+}
+
+public class TimeMap
+{
+    private Dictionary<string, SortedDictionary<int, string>> _values;
+    
+    /** Initialize your data structure here. */
+    public TimeMap() {
+        _values = new Dictionary<string, SortedDictionary<int, string>>();
+    }
+    
+    public void Set(string key, string value, int timestamp)
+    {
+        var data = _values.ContainsKey(key) ? _values[key] : new SortedDictionary<int, string>();
+            data[timestamp] = value;
+
+        _values[key] = data;
+    }
+    
+    public string Get(string key, int timestamp) {
+        if (!_values.ContainsKey(key))
+        {
+            return string.Empty;
+        }
+
+        var data = _values[key];
+        if (data.ContainsKey(timestamp))
+        {
+            return data[timestamp];
+        }
+
+        var tsKey = timestamp;
+
+        var keys = data.Keys;
+        var i = keys.Count - 1;
+        
+        while (i >= 0)
+        {
+            if (timestamp > keys.ElementAt(i))
+            {
+                tsKey = keys.ElementAt(i);
+                break;
+            }
+
+            i--;
+        }
+
+        return data.ContainsKey(tsKey) ? data[tsKey] : string.Empty;
+    }
+}
---
+++ VerticalOrderTraversalOfABinaryTree/VerticalOrderTraversalOfABinaryTree/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerticalOrderTraversalOfABinaryTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result1 = new Solution().VerticalTraversal(
+                new TreeNode(3)
+                {
+                    left = new TreeNode(9),
+                    right = new TreeNode(20)
+                    {
+                        left = new TreeNode(15),
+                        right = new TreeNode(7)
+                    }
+                });
+
+            Print(result1);
+            
+            var result2 = new Solution().VerticalTraversal(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(4),
+                        right = new TreeNode(5),
+                    },
+                    right = new TreeNode(3)
+                    {
+                        left = new TreeNode(6),
+                        right = new TreeNode(7)
+                    }
+                });
+
+            Print(result2);
+        }
+
+        private static void Print(IList<IList<int>> list)
+        {
+            foreach (var sublist in list)
+            {
+                Console.WriteLine();
+
+                foreach (var i in sublist)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public IList<IList<int>> VerticalTraversal(TreeNode root) {
+        var locations = new List<Location>();
+        DFS(root, 0, 0, locations);
+        locations.Sort((a, b) =>
+        {
+            if (a.X != b.X)
+            {
+                return a.X.CompareTo(b.X);
+            }
+            else if (a.Y != b.Y)
+            {
+                return a.Y.CompareTo(b.Y);
+            }
+            else
+            {
+                return a.Value.CompareTo(b.Value);
+            }
+        });
+        
+        var result = new List<IList<int>>();
+            result.Add(new List<int>());
+
+        var prev = locations[0].X;
+        foreach (var location in locations)
+        {
+            if (location.X != prev)
+            {
+                prev = location.X;
+                result.Add(new List<int>());
+            }
+            
+            result[result.Count - 1].Add(location.Value);
+        }
+        
+        return result;
+    }
+
+    private void DFS(TreeNode root, int x, int y, List<Location> list)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        
+        list.Add(new Location { X = x, Y = y, Value = root.val});
+        
+        DFS(root.left, x-1, y+1, list);
+        DFS(root.right, x+1, y+1, list);
+    }
+
+    private class Location
+    {
+        public int X;
+        public int Y;
+        public int Value;
+    }
+}
