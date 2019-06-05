625039355ebf1afdb9e9d0894623072c1d234b86
Timur Koshel <t.koshel@come2play.com>
2019-06-05 14:28:59 +0300

    custom sort string

---
+++ CustomSortString/CustomSortString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace CustomSortString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().CustomSortString("cba", "abcd"));
+        }
+    }
+}
+
+public class Solution {
+    public string CustomSortString(string S, string T)
+    {
+        var result = new List<string>();
+
+        foreach (var currentChar in S)
+        {
+            var str = string.Empty;
+            var count = T.Count(c => c == currentChar);
+            
+            for (var i = 0; i < count; i++)
+            {
+                str += currentChar;
+            }
+            
+            result.Add(str);
+        }
+
+        foreach (var currentChar in T)
+        {
+            if (!S.Contains(currentChar))
+            {
+                result.Add(currentChar.ToString());
+            }
+        }
+
+        return string.Join("", result);
+    }
+}
