087fa68a080f09f2034be7acc47917f13e516e25
Timur Koshel <t.koshel@come2play.com>
2019-06-05 15:07:39 +0300

    Min depth of binary tree

---
+++ MinDepthOfBinaryTree/MinDepthOfBinaryTree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MinDepthOfBinaryTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().MinDepth(new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20)
+                {
+                    left = new TreeNode(15),
+                    right = new TreeNode(7),
+                }
+            }));
+            
+            Console.WriteLine(new Solution().MinDepth(new TreeNode(1)
+            {
+                left = new TreeNode(2)
+            }));
+            
+            Console.WriteLine(new Solution().MinDepth(new TreeNode(3)));
+            Console.WriteLine(new Solution().MinDepth(null));
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        if (root.left != null && root.right != null)
+        {
+            return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;            
+        } else if (root.left == null)
+        {
+            return MinDepth(root.right) + 1;
+        }
+        else if (root.right == null)
+        {
+            return MinDepth(root.left) + 1;
+        }
+
+        return 0;
+    }
+}
