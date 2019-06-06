bdb49cb6b5fe34df46d4311d83993a3fe252becb
Timur Koshel <t.koshel@come2play.com>
2019-06-06 13:40:52 +0300

    Best time to buy and sell stock 2 and valid palindrome

---
+++ BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStockII
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().MaxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+            Console.WriteLine(new Solution().MaxProfit(new int[] {7, 6, 4, 3, 1}));
+            Console.WriteLine(new Solution().MaxProfit(new int[] {1, 2, 3, 4, 5}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var result = 0;
+
+        var min = int.MaxValue;
+        var max = 0;
+
+        for (var i = 0; i < prices.Length; i++)
+        {
+            var price = prices[i];
+
+            if (price < min)
+            {
+                min = price;
+            }
+            else if (max < price - min)
+            {
+                max = price;
+                result += max - min;
+
+                i--;
+                
+                min = int.MaxValue;
+                max = 0;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ ValidPalindrome/ValidPalindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidPalindrome
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsPalindrome("A man, a plan, a canal: Panama"));
+            Console.WriteLine(false == new Solution().IsPalindrome("race a car"));
+            Console.WriteLine(false == new Solution().IsPalindrome("0P"));
+            Console.WriteLine(false == new Solution().IsPalindrome("A"));
+        }
+    }
+}
+
+public class Solution {
+    public bool IsPalindrome(string s)
+    {
+        var clean = Regex.Replace(s, "[^0-9a-zA-Z]", string.Empty);
+        if (clean.Length < 2)
+        {
+            return true;
+        }
+
+        clean = clean.ToLower();
+
+        var len = clean.Length;
+        
+        for (var i = 0; i < len; i++)
+        {
+            if (clean[i] != clean[len - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
