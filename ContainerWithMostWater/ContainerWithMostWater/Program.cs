5ed8cbc6cc1b7f12f81b2075225d6452415b3b35
Timur Koshel <t.koshel@come2play.com>
2019-06-07 12:17:11 +0300

    Single Number and container with most water

---
+++ ContainerWithMostWater/ContainerWithMostWater/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ContainerWithMostWater
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(49 == new Solution2().MaxArea(new []{ 1, 8, 6, 2, 5, 4, 8, 3, 7}));
+            Console.WriteLine(1 == new Solution2().MaxArea(new []{ 1, 8 }));
+            Console.WriteLine(0 == new Solution2().MaxArea(new []{ 0, 2 }));
+            Console.WriteLine(2 == new Solution2().MaxArea(new []{ 1, 2, 1 }));
+        }
+    }
+}
+
+public class Solution {
+    public int MaxArea(int[] height)
+    {
+        var result = 0;
+
+        for (var i = 0; i < height.Length; i++)
+        {
+            for (var j = i + 1; j < height.Length; j++)
+            {
+                var a = height[i];
+                var b = height[j];
+
+                var minHeight = Math.Min(a, b);
+                var size = minHeight * (j - i);
+
+                if (size > result)
+                {
+                    result = size;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+public class Solution2 {
+    public int MaxArea(int[] height)
+    {
+        var result = 0;
+        var l = 0;
+        var r = height.Length - 1;
+        
+        while (l < r)
+        {
+            result = Math.Max(result, Math.Min(height[l], height[r]) * (r - l));
+
+            if (height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ SingleNumber/SingleNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SingleNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().SingleNumber(new []{2, 2, 1}));
+            Console.WriteLine(4 == new Solution().SingleNumber(new []{4, 1, 2, 1, 2}));
+        }
+    }
+}
+
+public class Solution {
+    public int SingleNumber(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            dict[num] = dict.ContainsKey(num) ? dict[num] + 1 : 1;
+        }
+
+        return dict.Aggregate((x,y) => x.Value < y.Value ? x : y).Key;
+    }
+}
