910f904205d6f57d73547132da1ec99a2af69852
Timur Koshel <t.koshel@come2play.com>
2019-06-10 15:34:42 +0300

    Linked list cycle

---
+++ LinkedListCycle/LinkedListCycle/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListCycle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var head = new ListNode(3);
+            var cycle = new ListNode(2);
+
+            head.next = cycle;
+            cycle.next = new ListNode(0)
+            {
+                next = new ListNode(-4)
+                {
+                    next = cycle,
+                }
+            };
+            
+            Console.WriteLine(true == new Solution().HasCycle(head));
+        }
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var set = new HashSet<ListNode>();
+        var next = head;
+
+        while (next != null)
+        {
+            if (set.Contains(next))
+            {
+                return true;
+            }
+
+            set.Add(next);
+            next = next.next;
+        }
+
+        return false;
+    }
+}
