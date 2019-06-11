9a2b4022c41a2a1fcd3eee48c2d09e5d2f1304d0
Timur Koshel <t.koshel@come2play.com>
2019-06-11 16:37:17 +0300

    Excel Sheet Column Title

---
+++ ExcelSheetColumnTitle/ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExcelSheetColumnTitle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("A" == new Solution().ConvertToTitle(1));
+            Console.WriteLine("Z" == new Solution().ConvertToTitle(26));
+            Console.WriteLine("AB" == new Solution().ConvertToTitle(28));
+            Console.WriteLine("ZY" == new Solution().ConvertToTitle(701));
+            Console.WriteLine("ZZ" == new Solution().ConvertToTitle(702));
+        }
+    }
+}
+
+public class Solution
+{
+    public string ConvertToTitle(int n)
+    {
+        var stack = new Stack<char>();
+        
+        while (n > 0)
+        {
+            n--;
+            stack.Push((char)('A' + n % 26));
+            n /= 26;
+        }
+        
+        return new string(stack.ToArray());
+    }
+}
