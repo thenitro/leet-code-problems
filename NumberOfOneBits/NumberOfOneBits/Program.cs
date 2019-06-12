789419f7826d169fee787b01edac7f7d0c3510e5
Timur Koshel <t.koshel@come2play.com>
2019-06-12 16:15:04 +0300

    number of one bits

---
+++ NumberOfOneBits/NumberOfOneBits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+
+namespace NumberOfOneBits
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().HammingWeight(11));
+            Console.WriteLine(31 == new Solution().HammingWeight(4294967293));
+        }
+    }
+}
+
+public class Solution {
+    public int HammingWeight(uint n)
+    {
+        var bits = 0;
+        var mask = 1;
+
+        for (var i = 0; i < 32; i++)
+        {
+            if ((n & mask) != 0)
+            {
+                bits++;
+            }
+
+            mask <<= 1;
+        }
+
+        return bits;
+    }
+}
