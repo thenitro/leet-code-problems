dc0ed972091dea8df747e4b245458612f758321a
Timur Koshel <t.koshel@come2play.com>
2019-06-12 10:18:13 +0300

    Excel Sheet Column Number, Factorial Trailing Zeros

---
+++ ExcelSheetColumnNumber/ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ExcelSheetColumnNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().TitleToNumber("A"));
+            Console.WriteLine(28 == new Solution().TitleToNumber("AB"));
+            Console.WriteLine(701 == new Solution().TitleToNumber("ZY"));
+        }
+    }
+}
+
+public class Solution
+{
+    private Dictionary<char, int> Map = new Dictionary<char, int>()
+    {
+        {'A', 1},
+        {'B', 2},
+        {'C', 3},
+        {'D', 4},
+        {'E', 5},
+        {'F', 6},
+        {'G', 7},
+        {'H', 8},
+        {'I', 9},
+        {'J', 10},
+        {'K', 11},
+        {'L', 12},
+        {'M', 13},
+        {'N', 14},
+        {'O', 15},
+        {'P', 16},
+        {'Q', 17},
+        {'R', 18},
+        {'S', 19},
+        {'T', 20},
+        {'U', 21},
+        {'V', 22},
+        {'W', 23},
+        {'X', 24},
+        {'Y', 25},
+        {'Z', 26},
+    };
+    
+    public int TitleToNumber(string s)
+    {
+        var result = 0;
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            var c = s[i];
+            var x = (int)(Map[c] * Math.Pow(26, s.Length - i - 1));
+
+            result += x;
+        }
+        
+        return result;
+    }
+}
---
+++ FactorialTrailingZeroes/FactorialTrailingZeroes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FactorialTrailingZeroes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(0 == new Solution().TrailingZeroes(3));
+            Console.WriteLine(1 == new Solution().TrailingZeroes(5));
+            Console.WriteLine(0 == new Solution().TrailingZeroes(0));
+            Console.WriteLine(1 == new Solution().TrailingZeroes(6));
+            Console.WriteLine(2 == new Solution().TrailingZeroes(13));
+            Console.WriteLine(7 == new Solution().TrailingZeroes(30));
+        }
+    }
+}
+
+public class Solution {
+    public int TrailingZeroes(int n)
+    {
+        var count = 0;
+
+        while (n != 0)
+        {
+            var tmp = n / 5;
+            count += tmp;
+            n = tmp;
+        }
+
+        return count;
+    }
+}
