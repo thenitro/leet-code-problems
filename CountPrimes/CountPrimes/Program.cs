96697b8ea80bfe5157be35f97ad4bb32ba95cc23
Timur Koshel <t.koshel@come2play.com>
2019-06-13 11:48:29 +0300

    count primes

---
+++ CountPrimes/CountPrimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CountPrimes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(4 == new Solution().CountPrimes(10));
+        }
+    }
+}
+
+public class Solution {
+    public int CountPrimes(int n)
+    {
+        var notPrime = new bool[n];
+
+        var count = 0;
+        for (var i = 2; i < n; i++)
+        {
+            if (notPrime[i] == false)
+            {
+                count++;
+
+                for (var j = 2; i * j < n; j++)
+                {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+}
