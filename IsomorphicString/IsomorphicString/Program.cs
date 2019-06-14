3a21b95e1d0df04e05f632b236828476cb6c63f4
Timur Koshel <t.koshel@come2play.com>
2019-06-14 10:50:54 +0300

    IsomorphicString

---
+++ IsomorphicString/IsomorphicString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace IsomorphicString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().IsIsomorphic("egg", "add"));
+            Console.WriteLine(new Solution().IsIsomorphic("foo", "bar"));
+            Console.WriteLine(new Solution().IsIsomorphic("paper", "title"));
+            Console.WriteLine(new Solution().IsIsomorphic("ab", "aa"));
+        }
+    }
+}
+
+public class Solution {
+    public bool IsIsomorphic(string s, string t)
+    {
+        var dict = new Dictionary<char, char>();
+        var dictT = new Dictionary<char, char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var sChar = s[i];
+            var tChar = t[i];
+
+            if (dict.ContainsKey(sChar))
+            {
+                if (!dictT.ContainsKey(tChar) || dictT[tChar] != sChar)
+                {
+                    return false;
+                }
+            } else if (dictT.ContainsKey(tChar))
+            {
+                if (!dict.ContainsKey(sChar) || dict[sChar] != tChar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                dict[sChar] = tChar;
+                dictT[tChar] = sChar;
+            }
+        }
+
+        return true;
+    }
+}
