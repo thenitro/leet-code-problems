a244cbb0708b9154a1a70d8215e1fcd09b259654
Timur Koshel <t.koshel@come2play.com>
2019-06-18 11:26:42 +0300

    contains duplicate

---
+++ ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainsDuplicate
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().ContainsDuplicate(new int[] {1, 2, 3, 1}));
+            Console.WriteLine(false == new Solution().ContainsDuplicate(new int[] {1, 2, 3, 4}));
+            Console.WriteLine(true == new Solution().ContainsDuplicate(new int[] {1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var set = new HashSet<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (set.Contains(nums[i]))
+            {
+                return true;
+            }
+
+            set.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
