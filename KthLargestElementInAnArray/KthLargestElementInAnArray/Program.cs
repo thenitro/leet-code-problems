77b06c6ec4e3179baef066ee2321eede4515d94d
Timur Koshel <t.koshel@come2play.com>
2019-06-19 11:02:53 +0300

    Kth Largest Element In An Array

---
+++ KthLargestElementInAnArray/KthLargestElementInAnArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KthLargestElementInAnArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().FindKthLargest(new int[] { 3,2,1,5,6,4 }, 2));
+            Console.WriteLine(4 == new Solution().FindKthLargest(new int[] { 3,2,3,1,2,4,5,5,6 }, 4));
+        }
+    }
+}
+
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        Array.Sort(nums);
+
+        return nums[nums.Length - k];
+    }
+}
