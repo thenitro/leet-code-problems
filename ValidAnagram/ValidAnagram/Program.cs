930c5f1c869a00b3805cec86754c04be06798f37
Timur Koshel <timurkoshel@gmail.com>
2019-06-23 11:54:31 +0300

    Valid anagram

---
+++ ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidAnagram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsAnagram("anagram", "nagaram"));
+            Console.WriteLine(false == new Solution().IsAnagram("rat", "car"));
+            Console.WriteLine(false == new Solution().IsAnagram("ab", "a"));
+        }
+    }
+}
+
+public class Solution {
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        
+        var dict = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            dict[c] = dict.ContainsKey(c) ? dict[c] + 1 : 1;
+        }
+
+        foreach (var c in t)
+        {
+            if (!dict.ContainsKey(c) || dict[c] == 0)
+            {
+                return false;
+            }
+
+            dict[c] -= 1;
+        }
+
+        return true;
+    }
+}
