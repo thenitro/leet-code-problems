799a777a4cd4bd3d20eeca11f8e18c1e60b93c5c
Timur Koshel <timurkoshel@gmail.com>
2019-06-23 12:43:33 +0300

    Add digits, Ugly number

---
+++ AddDigits/AddDigits/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AddDigits
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().AddDigits(38));
+        }
+    }
+}
+
+public class Solution {
+    public int AddDigits(int num)
+    {
+        var s = num.ToString();
+        var sum = int.Parse(s);
+
+        while (s.Length > 1)
+        {
+            sum = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                sum += c - '0';
+            }
+
+            s = sum.ToString();
+        }
+
+        return sum;
+    }
+}
---
+++ UglyNumber/UglyNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UglyNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsUgly(6));
+            Console.WriteLine(true == new Solution().IsUgly(8));
+            Console.WriteLine(false == new Solution().IsUgly(14));
+            Console.WriteLine(false == new Solution().IsUgly(0));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool IsUgly(int num)
+    {
+        while (num > 0)
+        {
+            if (num == 1)
+            {
+                return true;
+            }
+        
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+            } else if (num % 3 == 0)
+            {
+                num = num / 3;
+            } else if (num % 5 == 0)
+            {
+                num = num / 5;
+            }
+            else
+            {
+                break;
+            }
+        }        
+
+        return false;
+    }    
+}
