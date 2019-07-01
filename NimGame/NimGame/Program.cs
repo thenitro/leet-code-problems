9fd378e014946e00b9225ba0771b39041826711c
Timur Koshel <t.koshel@come2play.com>
2019-07-01 10:41:23 +0300

    Nim Game

---
+++ NimGame/NimGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NimGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(false == new Solution().CanWinNim(4));
+            Console.WriteLine(true == new Solution().CanWinNim(5));
+            Console.WriteLine(false == new Solution().CanWinNim(1348820612));
+        }
+    }
+}
+
+public class Solution {
+    public bool CanWinNim(int n)
+    {
+        return n % 4 != 0;
+    }
+}
