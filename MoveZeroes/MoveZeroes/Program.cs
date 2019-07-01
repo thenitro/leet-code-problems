df3f51f9ee1ffc9440b5dd5a9506aa01e1f06e7c
Timur Koshel <t.koshel@come2play.com>
2019-07-01 09:44:16 +0300

    move zeroes, swap nodes in pairs

---
+++ MoveZeroes/MoveZeroes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MoveZeroes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new int[] {0, 1, 0, 3, 12};
+
+            new Solution().MoveZeroes(arr);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            
+            Console.WriteLine();
+            
+            arr = new int[] {0, 0, 1};
+
+            new Solution().MoveZeroes(arr);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var zeroOffset = 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > nums.Length - zeroOffset)
+            {
+                return;
+            }
+            
+            if (nums[i] == 0)
+            {
+                var j = i;
+                
+                while (j < nums.Length - zeroOffset)
+                {
+                    nums[j] = nums[j + 1];
+                    j++;
+                }
+                
+                nums[nums.Length - zeroOffset] = 0;
+                
+                zeroOffset++;
+
+                i--;
+            }
+        }
+    }
+}
---
+++ SwapNodesInPairs/SwapNodesInPairs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SwapNodesInPairs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var head = new Solution().SwapPairs(new ListNode(1)
+                {next = new ListNode(2) {next = new ListNode(3) {next = new ListNode(4)}}});
+
+            while (head != null)
+            {
+                Console.Write(head.val + " ");
+                
+                head = head.next;
+            }
+        }
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        var next = head.next;
+
+        head.next = SwapPairs(head.next.next);
+        next.next = head;
+
+        return next;
+    }
+}
