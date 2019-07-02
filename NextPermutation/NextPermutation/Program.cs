de8a582c83f4e1e90133d6dbea596affdf39a738
Timur Koshel <t.koshel@come2play.com>
2019-07-02 10:19:45 +0300

    Reverse string, next permutation

---
+++ NextPermutation/NextPermutation/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace NextPermutation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new int[] {1, 2, 3};
+            new Solution().NextPermutation(arr);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new int[] {3, 2, 1};
+            new Solution().NextPermutation(arr);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new int[] {1, 1, 5};
+            new Solution().NextPermutation(arr);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new int[] {1, 3, 2};
+            new Solution().NextPermutation(arr);
+
+            foreach (var i in arr)//2 1 3
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Solution
+{
+    public void NextPermutation(int[] nums)
+    {
+        var i = nums.Length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i])
+        {
+            i--;
+        }
+
+        if (i >= 0)
+        {
+            var j = nums.Length - 1;
+            while (j >= 0 && nums[j] <= nums[i])
+            {
+                j--;
+            }
+
+            Swap(nums, i, j);
+        }
+
+        Reverse(nums, i + 1);
+    }
+
+    private void Swap(int[] nums, int i, int j)
+    {
+        var temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void Reverse(int[] nums, int start)
+    {
+        var i = start;
+        var j = nums.Length - 1;
+
+        while (i < j)
+        {
+            Swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new char[] {'h', 'e', 'l', 'l', 'o'};
+            
+            new Solution().ReverseString(arr);
+
+            foreach (var c in arr)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new char[] {'H', 'a', 'n', 'n', 'a', 'h'};
+            
+            new Solution().ReverseString(arr);
+            
+            foreach (var c in arr)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new char[] {'H'};
+            
+            new Solution().ReverseString(arr);
+            
+            foreach (var c in arr)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new char[] {'H', 'R'};
+            
+            new Solution().ReverseString(arr);
+            
+            foreach (var c in arr)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            
+            arr = new char[] {};
+            
+            new Solution().ReverseString(arr);
+            
+            foreach (var c in arr)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Solution {
+    public void ReverseString(char[] s)
+    {
+        if (s.Length == 0 || s.Length == 1)
+        {
+            return;
+        }
+        
+        var middle = s.Length / 2;
+
+        for (var i = 0; i < middle; i++)
+        {
+            var end = s.Length - (i + 1); 
+            
+            var tmp = s[i];
+            s[i] = s[end];
+            s[end] = tmp;
+        }
+    }
+}
