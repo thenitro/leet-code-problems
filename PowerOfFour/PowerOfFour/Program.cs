2f503c7bdb5a801abe57bc26a1455ead35fe0eee
Timur Koshel <t.koshel@come2play.com>
2019-07-02 08:53:20 +0300

    Power of four

---
+++ PowerOfFour/PowerOfFour/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PowerOfFour
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsPowerOfFour(16));
+            Console.WriteLine(false == new Solution().IsPowerOfFour(5));
+        }
+    }
+}
+
+public class Solution {
+    public bool IsPowerOfFour(int num) {
+        while (true)
+        {
+            if (num == 1)
+            {
+                return true;
+            }
+
+            if (num == 0 || num % 4 != 0)
+            {
+                return false;
+            }
+
+            num /= 4;
+        }
+    }
+}
