9eeb745e7413689629e97c7e66cc53466b8a879f
Timur Koshel <t.koshel@come2play.com>
2019-07-03 11:33:53 +0300

    Intersection of two arrays 2

---
+++ IntersectionOfTwoArrays2/IntersectionOfTwoArrays2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntersectionOfTwoArrays2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().Intersect(new []{1, 2, 2, 1}, new []{2,2});
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            
+            result = new Solution().Intersect(new []{4, 9, 5}, new []{9,4,9,8,4});
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>();
+        
+        var dict1 = new Dictionary<int, int>();
+        var dict2 = new Dictionary<int, int>();
+
+        foreach (var i in nums1)
+        {
+            dict1[i] = dict1.ContainsKey(i) ? dict1[i] + 1 : 1;
+        }
+        
+        foreach (var i in nums2)
+        {
+            dict2[i] = dict2.ContainsKey(i) ? dict2[i] + 1 : 1;
+        }
+
+        foreach (var key in dict1.Keys)
+        {
+            if (dict2.ContainsKey(key))
+            {
+                var times = Math.Min(dict1[key], dict2[key]);
+
+                while (times > 0)
+                {
+                    result.Add(key);
+                    times--;
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
