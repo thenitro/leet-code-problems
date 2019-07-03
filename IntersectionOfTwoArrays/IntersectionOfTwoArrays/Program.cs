8bc0969baedf613bb56b88ca55b9897337d82e97
Timur Koshel <t.koshel@come2play.com>
2019-07-03 10:04:38 +0300

    Intersection of two arrays, reverse vowels of string

---
+++ IntersectionOfTwoArrays/IntersectionOfTwoArrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntersectionOfTwoArrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().Intersection(new []{1, 2, 2, 1}, new []{2,2});
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            
+            result = new Solution().Intersection(new []{4, 9, 5}, new []{9,4,9,8,4});
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>();
+
+        for (var i = 0; i < nums1.Length; i++)
+        {
+            var iElement = nums1[i];
+            
+            for (int j = 0; j < nums2.Length; j++)
+            {
+                if (iElement == nums2[j] && result.IndexOf(iElement) == -1)
+                {
+                    result.Add(iElement);
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ ReverseVowelsOfString/ReverseVowelsOfString/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ReverseVowelsOfString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("holle" == new Solution().ReverseVowels("hello"));
+            Console.WriteLine("leotcede" == new Solution().ReverseVowels("leetcode"));
+            Console.WriteLine("eo" == new Solution().ReverseVowels("oe"));
+            Console.WriteLine("ll" == new Solution().ReverseVowels("ll"));
+            Console.WriteLine("Aa" == new Solution().ReverseVowels("aA"));
+        }
+    }
+}
+
+public class Solution {
+    public string ReverseVowels(string s)
+    {
+        var arr = s.ToCharArray();
+        
+        var lo = 0;
+        var hi = s.Length - 1;
+
+        while (lo < hi)
+        {
+            while (!IsVowel(s[lo]) && lo < hi)
+            {
+                lo++;
+            }
+
+            while (!IsVowel(s[hi]) && lo < hi)
+            {
+                hi--;
+            }
+
+            if (IsVowel(s[lo]) && IsVowel(s[hi]))
+            {
+                var temp = arr[lo];
+                arr[lo] = arr[hi];
+                arr[hi] = temp;
+            }
+
+            lo++;
+            hi--;
+        }
+        
+        return new string(arr);
+    }
+
+    private bool IsVowel(char c)
+    {
+        switch (Char.ToLower(c))
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+        }
+
+        return false;
+    }
+}
