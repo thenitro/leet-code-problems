25afd96d2a34b6cb3f7d1d38b5d2d79dd72d9be1
Timur Koshel <t.koshel@come2play.com>
2019-07-03 11:53:38 +0300

    Valid perfect square

---
+++ ValidPerfectSquare/ValidPerfectSquare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ValidPerfectSquare
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsPerfectSquare(9));
+            Console.WriteLine(true == new Solution().IsPerfectSquare(16));
+            Console.WriteLine(false == new Solution().IsPerfectSquare(14));
+            Console.WriteLine(true == new Solution().IsPerfectSquare(1));
+            Console.WriteLine(false == new Solution().IsPerfectSquare(2147483647));
+            Console.WriteLine(true == new Solution().IsPerfectSquare(100));
+        }
+    }
+}
+
+public class Solution {
+    public bool IsPerfectSquare(int num)
+    {
+        long x = num;
+
+        while (x * x > num)
+        {
+            x = (x + num / x) >> 1;
+        }
+
+        return x * x == num;
+    }
+}
