f310d3f844ab41179d294316d1a5caf16dc535cc
Timur Koshel <t.koshel@come2play.com>
2019-07-05 13:12:42 +0300

    ransom note, increasing triplet subsequence

---
+++ IncreasingTripletSubsequence/IncreasingTripletSubsequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace IncreasingTripletSubsequence
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(true == new Solution().IncreasingTriplet(new []{ 1, 2, 3, 4, 5 }));
+			Console.WriteLine(false == new Solution().IncreasingTriplet(new []{ 5, 4, 3, 2, 1 }));
+			Console.WriteLine(true == new Solution().IncreasingTriplet(new []{ 2, 1, 5, 0, 4, 6 }));
+			Console.WriteLine(true == new Solution().IncreasingTriplet(new []{ 5, 1, 5, 5, 2, 5, 4 }));
+			Console.WriteLine(false == new Solution().IncreasingTriplet(new []{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 }));
+		}
+	}
+}
+
+public class Solution {
+ 	public bool IncreasingTriplet(int[] nums)
+    {
+	    var c1 = int.MaxValue;
+	    var c2 = int.MaxValue;
+
+	    for (var i = 0; i < nums.Length; i++)
+	    {
+		    var x = nums[i];
+
+		    if (x <= c1)
+		    {
+			    c1 = x;
+		    }
+		    else if (x <= c2)
+		    {
+			    c2 = x;
+		    }
+		    else
+		    {
+			    return true;
+		    }
+		}
+
+	    return false;
+    }
+ }
