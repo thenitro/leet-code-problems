4e15541833d26072e5358ca14d375bb557425e9d
Timur Koshel <t.koshel@come2play.com>
2019-07-08 15:19:40 +0300

    valid palindrome 2

---
+++ ValidPalindrome2/ValidPalindrome2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidPalindrome2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().ValidPalindrome("aba"));
+            Console.WriteLine(true == new Solution().ValidPalindrome("abca"));
+            Console.WriteLine(true == new Solution().ValidPalindrome("acbca"));
+            Console.WriteLine(true == new Solution().ValidPalindrome("abbda"));
+            Console.WriteLine(false == new Solution().ValidPalindrome("acbda"));
+            Console.WriteLine(false == new Solution().ValidPalindrome("abc"));
+            Console.WriteLine(true == new Solution().ValidPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"));
+            Console.WriteLine(true == new Solution().ValidPalindrome("aguokepatgbnvfqmgmlucupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuclmgmqfvnbgtapekouga"));
+        }
+    }
+}
+
+public class Solution {
+    public bool ValidPalindrome(string s)
+    {
+        var middle = s.Length / 2;
+
+        for (var i = 0; i < middle; i++)
+        {
+            var j = s.Length - 1 - i;
+            
+            if (s[i] != s[j])
+            {
+                return IsPalindromeRange(s, i + 1, j) || IsPalindromeRange(s, i, j - 1);
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsPalindromeRange(string s, int startIndex, int endIndex)
+    {
+        for (var i = startIndex; i <= startIndex + (endIndex - startIndex) / 2; i++)
+        {
+            if (s[i] != s[endIndex - i + startIndex])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
