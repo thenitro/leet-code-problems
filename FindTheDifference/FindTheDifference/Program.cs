c67430376cdacca7a3b96fdf0691cb4a77c689db
Timur Koshel <t.koshel@come2play.com>
2019-07-10 10:01:51 +0300

    Find the difference, first unique character in string

---
+++ FindTheDifference/FindTheDifference/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+
+namespace FindTheDifference
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine('e' == new Solution().FindTheDifference("abcd", "abcde"));
+            Console.WriteLine('a' == new Solution().FindTheDifference("a", "aa"));
+        }
+    }
+}
+
+public class Solution {
+    public char FindTheDifference(string s, string t)
+    {
+        var dict = new Dictionary<char, int>();
+
+        var big = s.Length > t.Length ? s : t;
+        var small = s.Length > t.Length ? t : s;
+
+        foreach (var c in big)
+        {
+            dict[c] = dict.ContainsKey(c) ? dict[c] + 1 : 1;
+        }
+
+        foreach (var c in small)
+        {
+            dict[c] = dict.ContainsKey(c) ? dict[c] - 1 : 0;
+        }
+
+        foreach (var kv in dict)
+        {
+            if (kv.Value > 0)
+            {
+                return kv.Key;
+            }
+        }
+        
+        return Char.MinValue;
+    }
+}
---
+++ FirstUniqueCharacterInString/FirstUniqueCharacterInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstUniqueCharacterInString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(0 == new Solution().FirstUniqChar("leetcode"));
+            Console.WriteLine(2 == new Solution().FirstUniqChar("loveleetcode"));
+            Console.WriteLine(-1 == new Solution().FirstUniqChar(""));
+        }
+    }
+}
+
+public class Solution {
+    public int FirstUniqChar(string s)
+    {
+        var dictionary = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            dictionary[c] = dictionary.ContainsKey(c) ? dictionary[c] + 1 : 1;
+        }
+
+        foreach (var kv in dictionary)
+        {
+            if (kv.Value == 1)
+            {
+                return s.IndexOf(kv.Key);
+            }
+        }
+
+        return -1;
+    }
+}
