384ec6cad940a62f55f24462eb607ec8acc4ce5b
Timur Koshel <t.koshel@come2play.com>
2019-07-10 11:18:44 +0300

    Find first and last position of element in sorted array, valid sudoku

---
+++ FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FindFirstAndLastPositionOfElementInSortedArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().SearchRange(new []{ 5, 7, 7, 8, 8, 10}, 8);
+            Console.WriteLine(result[0] == 3 && result[1] == 4);
+            
+            result = new Solution().SearchRange(new []{ 5, 7, 7, 8, 10}, 8);
+            Console.WriteLine(result[0] == 3 && result[1] == 3);
+            
+            result = new Solution().SearchRange(new []{ 5, 7, 7, 8, 8, 10}, 6);
+            Console.WriteLine(result[0] == -1 && result[1] == -1);
+            
+            result = new Solution().SearchRange(new []{ 1 }, 1);
+            Console.WriteLine(result[0] == 0 && result[1] == 0);
+            
+            result = new Solution().SearchRange(new []{ 1 }, 2);
+            Console.WriteLine(result[0] == -1 && result[1] == -1);
+            
+            result = new Solution().SearchRange(new []{ 1, 4 }, 4);
+            Console.WriteLine(result[0] == 1 && result[1] == 1);
+        }
+    }
+}
+
+public class Solution {
+    public int[] SearchRange(int[] nums, int target)
+    {
+        if (nums.Length == 1 && nums[0] == target)
+        {
+            return new[] {0, 0};
+        }
+        
+        var lo = 0;
+        var hi = nums.Length;
+
+        while (lo < hi)
+        {
+            var middle = (lo + hi) / 2;
+            
+            if (target == nums[middle])
+            {
+                var left = middle;
+
+                while (left > 0 && nums[left - 1] == target)
+                {
+                    left--;
+                }
+                
+                var right = middle;
+
+                while (right < nums.Length - 1 && nums[right + 1] == target)
+                {
+                    right++;
+                }
+                
+                return new[] { left, right };
+            } 
+            
+            if (target > nums[middle])
+            {
+                lo = middle + 1;
+            }
+            else
+            {
+                hi = middle;
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
---
+++ ValidSudoku/ValidSudoku/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidSudoku
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsValidSudoku(
+                                  new char[][]
+                                  {
+                                      new char[] {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                                      new char[] {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                                      new char[] {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                                      new char[] {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                                      new char[] {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                                      new char[] {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                                      new char[] {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                                      new char[] {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                                      new char[] {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                                  }));
+
+            Console.WriteLine(false == new Solution().IsValidSudoku(
+                                  new char[][]
+                                  {
+                                      new char[] {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                                      new char[] {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                                      new char[] {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                                      new char[] {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                                      new char[] {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                                      new char[] {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                                      new char[] {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                                      new char[] {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                                      new char[] {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                                  }));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            for (var j = 0; j < 3; j++)
+            {
+                var set = new HashSet<int>();
+
+                for (var n = 0; n < 3; n++)
+                {
+                    for (var m = 0; m < 3; m++)
+                    {
+                        var character = board[i * 3 + n][j * 3 + m];
+                        if (character == '.')
+                        {
+                            continue;
+                        }
+
+                        if (set.Contains(character))
+                        {
+                            return false;
+                        }
+
+                        set.Add(character);
+                    }
+                }
+            }
+        }
+
+        for (var i = 0; i < 9; i++)
+        {
+            var set = new HashSet<int>();
+
+            for (var j = 0; j < 9; j++)
+            {
+                var character = board[i][j];
+                if (character == '.')
+                {
+                    continue;
+                }
+
+                if (set.Contains(character))
+                {
+                    return false;
+                }
+
+                set.Add(character);
+            }
+        }
+
+        for (var i = 0; i < 9; i++)
+        {
+            var set = new HashSet<int>();
+
+            for (var j = 0; j < 9; j++)
+            {
+                var character = board[j][i];
+                if (character == '.')
+                {
+                    continue;
+                }
+
+                if (set.Contains(character))
+                {
+                    return false;
+                }
+
+                set.Add(character);
+            }
+        }
+
+        return true;
+    }
+}
