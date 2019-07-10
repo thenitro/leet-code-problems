f9549f6d4467f4b773473dd1d27470894bd48bc6
Timur Koshel <t.koshel@come2play.com>
2019-07-10 09:33:25 +0300

    RansomNote

--- RansomNote/RansomNote/Program.cs
+++ RansomNote/RansomNote/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace RansomNote
+﻿using System;
+using System.Collections.Generic;
+
+namespace RansomNote
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(false == new Solution().CanConstruct("a", "b"));
+            Console.WriteLine(false == new Solution().CanConstruct("aa", "ab"));
+            Console.WriteLine(true == new Solution().CanConstruct("aa", "aab"));
+            Console.WriteLine(true == new Solution().CanConstruct("aa", "baab"));
+            Console.WriteLine(true == new Solution().CanConstruct("bjaajgea",
+                                  "affhiiicabhbdchbidghccijjbfjfhjeddgggbajhidhjchiedhdibgeaecffbbbefiabjdhggihccec"));
         }
+    }
+}
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var dictionary = new Dictionary<char, int>();
+
+        foreach (var c in magazine)
+        {
+            dictionary[c] = dictionary.ContainsKey(c) ? dictionary[c] + 1 : 1;
+        }
+
+        foreach (var c in ransomNote)
+        {
+            if (!dictionary.ContainsKey(c) || dictionary[c] < 1)
+            {
+                return false;
+            }
+
+            dictionary[c] = dictionary[c] - 1;
+        }
+
+        return true;
     }
 }
