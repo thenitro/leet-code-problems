92a670e1df0482e1ed58ac0df4c295469bf133ea
Timur Koshel <t.koshel@come2play.com>
2019-07-11 09:36:58 +0300

    Permutations and Permutation 2

---
+++ Permutations/Permutations/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().Permute(new []{ 1, 2, 3});
+
+            foreach (var i in result)
+            {
+                foreach (var j in i)
+                {
+                    Console.Write(j + " ");
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+public class Solution {
+    public IList<IList<int>> Permute(int[] nums) {
+        var results = new List<IList<int>>();
+        if (nums == null || nums.Length == 0)
+        {
+            return results;
+        }
+
+        Dfs(nums, new List<int>(), results);
+        
+        return results;
+    }
+
+    private void Dfs(int[] nums, List<int> temp, List<IList<int>> results)
+    {
+        if (temp.Count == nums.Length)
+        {
+            var result = new int[temp.Count];
+            temp.CopyTo(result);
+            results.Add(result);
+            return;
+        }
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (temp.Contains(nums[i]))
+            {
+                continue;
+            }
+            
+            temp.Add(nums[i]);
+            Dfs(nums, temp, results);
+            temp.RemoveAt(temp.Count - 1);
+        }
+    }
+}
---
+++ Permutations2/Permutations2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().PermuteUnique(new[] {1, 1, 2});
+            
+            foreach (var i in result)
+            {
+                foreach (var j in i)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        var results = new List<IList<int>>();
+        if (nums == null || nums.Length == 0)
+        {
+            return results;
+        }
+        
+        Array.Sort(nums);
+        Dfs(nums, new List<int>(), results, new bool[nums.Length]);
+
+        return results;
+    }
+
+    private void Dfs(int[] nums, List<int> temp, List<IList<int>> results, bool[] visited)
+    {
+        if (nums.Length == temp.Count)
+        {
+            var result = new int[temp.Count];
+            temp.CopyTo(result);
+            results.Add(result);
+            return;
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (visited[i])
+            {
+                continue;
+            }
+
+            visited[i] = true;
+            temp.Add(nums[i]);
+            Dfs(nums, temp, results, visited);
+            visited[i] = false;
+            temp.RemoveAt(temp.Count - 1);
+
+            while (i < nums.Length - 1 && nums[i] == nums[i + 1])
+            {
+                i++;
+            } 
+        }
+    }
+}
