ccd57902464d844259defdd46930b89f9fb5d4ce
Timur Koshel <t.koshel@come2play.com>
2019-07-15 08:46:27 +0300

    Nth digit

---
+++ NthDigit/NthDigit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NthDigit
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().FindNthDigit(3));
+            Console.WriteLine(0 == new Solution().FindNthDigit(11));
+            Console.WriteLine(4 == new Solution().FindNthDigit(323));
+            Console.WriteLine(1 == new Solution().FindNthDigit(232));
+            Console.WriteLine(1 == new Solution().FindNthDigit(1000000000));
+            Console.WriteLine(2 == new Solution().FindNthDigit(2147483647));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindNthDigit(int n)
+    {
+        long digits = 9;
+        long length = 1;
+        long first = 1;
+
+        while (n > length * digits)
+        {
+            n -= (int)(length * digits);
+            length++;
+            first *= 10;
+            digits *= 10;
+        }
+
+        first += (n - 1) / length;
+
+        var digitString = first.ToString();
+        return digitString[(int)((n - 1) % length)] - '0';
+    }
+}
