09fea9174fa3b44ce04bf4def2770de9dd9d7272
Timur Koshel <t.koshel@come2play.com>
2019-07-16 08:51:42 +0300

    Binary Watch, Sum of Left Leaves

---
+++ BinaryWatch/BinaryWatch/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace BinaryWatch
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().ReadBinaryWatch(1);
+            Console.WriteLine(string.Join(" ", result));
+            
+            var result2 = new Solution().ReadBinaryWatch(2);
+            Console.WriteLine(string.Join(" ", result2));
+            
+            var result4 = new Solution().ReadBinaryWatch(4);
+            Console.WriteLine(string.Join(" ", result4));
+        }
+    }
+}
+
+public class Solution {
+    public IList<string> ReadBinaryWatch(int num)
+    {
+        var combination = new int[10];
+        
+        for (var i = 0; i < num; i++)
+        {
+            combination[i] = 1;
+        }
+        
+        var results = new List<string>();
+
+        Dfs(combination, new List<int>(), results, new bool[combination.Length]);
+        
+        return results;
+    }
+
+    private string ConvertToTime(string binary)
+    {
+        var hours = Convert.ToInt32(binary.Substring(0, 4), 2);
+
+        if (hours > 11)
+        {
+            return string.Empty;
+        }
+
+        var minutes = Convert.ToInt32(binary.Substring(4, binary.Length - 4), 2);
+        if (minutes > 59)
+        {
+            return string.Empty;
+        }
+        
+        return hours + ":" + minutes.ToString("00");
+    }
+
+    private void Dfs(int[] combination, List<int> temp, List<string> results, bool[] visited)
+    {
+        if (temp.Count == combination.Length)
+        {
+            var result = ConvertToTime(string.Join("", temp));
+            if (string.IsNullOrEmpty(result))
+            {
+                return;
+            }
+            
+            results.Add(result);
+            return;
+        }
+
+        for (var i = 0; i < combination.Length; i++)
+        {
+            if (visited[i])
+            {
+                continue;
+            }
+
+            visited[i] = true;
+            temp.Add(combination[i]);
+            
+            Dfs(combination, temp, results, visited);
+            
+            visited[i] = false;
+            temp.RemoveAt(temp.Count - 1);
+
+            while (i < combination.Length - 1 && combination[i] == combination[i + 1])
+            {
+                i++;
+            }
+        }
+    }
+}
---
+++ SumOfLeftLeaves/SumOfLeftLeaves/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SumOfLeftLeaves
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var root = new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20)
+                {
+                    left = new TreeNode(15),
+                    right = new TreeNode(7)
+                }
+            };
+            
+            Console.WriteLine(24 == new Solution().SumOfLeftLeaves(root));
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public int SumOfLeftLeaves(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+        var sum = 0;
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (node == null)
+            {
+                continue;
+            }
+
+            if (node.left == null)
+            {
+                queue.Enqueue(node.right);
+            }
+            else
+            {
+                if (node.left.left == null && node.left.right == null)
+                {
+                    sum += node.left.val;
+                }
+                
+                queue.Enqueue(node.left);
+                queue.Enqueue(node.right);
+            }
+        }
+        
+        return sum;
+    }
+}
