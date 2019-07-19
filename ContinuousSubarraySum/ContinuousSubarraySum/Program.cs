8e49ca41afce95569b219d0baf51eb934d3247c8
Timur Koshel <t.koshel@come2play.com>
2019-07-19 11:12:22 +0300

    Continuous subarray sum, reconstruct original digits from english

---
+++ ContinuousSubarraySum/ContinuousSubarraySum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContinuousSubarraySum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().CheckSubarraySum(new []{23, 2, 4, 6, 7}, 6));
+            Console.WriteLine(true == new Solution().CheckSubarraySum(new []{23, 2, 6, 4, 7}, 6));
+        }
+    }
+}
+
+public class Solution {
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+        var map = new Dictionary<int, int>();
+            map.Add(0, -1);
+
+        var runningSum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            runningSum += nums[i];
+
+            if (k != 0)
+            {
+                runningSum %= k;
+            }
+
+            if (map.ContainsKey(runningSum))
+            {
+                if (1 - map[runningSum] > 1)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                map.Add(runningSum, i);
+            }
+        }
+
+        return false;
+    }
+}
---
+++ ReconstructOriginalDigitsFromEnglish/ReconstructOriginalDigitsFromEnglish/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReconstructOriginalDigitsFromEnglish
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("012" == new Solution().OriginalDigits("owoztneoer"));
+            Console.WriteLine("45" == new Solution().OriginalDigits("fviefuro"));
+        }
+    }
+}
+
+public class Solution {
+    public string OriginalDigits(string s)
+    {
+        var count = new int[10];
+
+        foreach (var c in s)
+        {
+            if (c == 'z') count[0]++;
+            if (c == 'w') count[2]++;
+            if (c == 'x') count[6]++;
+            if (c == 's') count[7]++;
+            if (c == 'g') count[8]++;
+            if (c == 'u') count[4]++;
+            if (c == 'f') count[5]++;
+            if (c == 'h') count[3]++;
+            if (c == 'i') count[9]++;
+            if (c == 'o') count[1]++;
+        }
+
+        count[7] -= count[6];
+        count[5] -= count[4];
+        count[3] -= count[8];
+        count[9] = count[9] - count[8] - count[5] - count[6];
+        count[1] = count[1] - count[0] - count[2] - count[4];
+
+        var sb = new StringBuilder();
+
+        for (var i = 0; i <= 9; i++)
+        {
+            for (var j = 0; j < count[i]; j++)
+            {
+                sb.Append(i);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
