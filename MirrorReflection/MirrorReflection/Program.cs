6d2243d438fdfefafe771136c3c83586c20a41de
Timur Koshel <t.koshel@come2play.com>
2019-08-07 09:20:24 +0300

    Mirror Reflection and Projection Area of 3d shapes

---
+++ MirrorReflection/MirrorReflection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MirrorReflection
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().MirrorReflection(2, 1));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MirrorReflection(int p, int q)
+    {
+        var g = Gcd(p, q);
+
+        p /= g;
+        p %= 2;
+        
+        q /= g;
+        q %= 2;
+
+        if (p == 1 && q == 1)
+        {
+            return 1;
+        }
+
+        return p == 1 ? 0 : 2;
+    }
+
+    private int Gcd(int a, int b)
+    {
+        if (a == 0)
+        {
+            return b;
+        }
+
+        return Gcd(b % a, a);
+    }
+}
---
+++ ProjectionAreaOf3DShapes/ProjectionAreaOf3DShapes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ProjectionAreaOf3DShapes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().ProjectionArea(new[] {new[] {2}}));
+            Console.WriteLine(17 == new Solution().ProjectionArea(new[] {new[] {1, 2}, new[] {3, 4}}));
+            Console.WriteLine(8 == new Solution().ProjectionArea(new[] {new[] {1, 0}, new[] {0, 2}}));
+            Console.WriteLine(14 == new Solution().ProjectionArea(new[] {new[] {1, 1, 1}, new[] {1, 0, 1}, new[] {1, 1, 1}}));
+            Console.WriteLine(21 == new Solution().ProjectionArea(new[] {new[] {2, 2, 2}, new[] {2, 1, 2}, new[] {2, 2, 2}}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int ProjectionArea(int[][] grid)
+    {
+        var l = grid.Length;
+        var z = 0;
+
+        for (var i = 0; i < l; i++)
+        {
+            for (var j = 0; j < l; j++)
+            {
+                if (grid[i][j] != 0)
+                {
+                    z++;
+                }
+            }
+        }
+
+        var x = 0;
+        for (var i = 0; i < l; i++)
+        {
+            var max = 0;
+            for (var j = 0; j < l; j++)
+            {
+                if (grid[i][j] > max)
+                {
+                    max = grid[i][j];
+                }
+            }
+
+            x += max;
+        }
+
+        var y = 0;
+        for (var j = 0; j < l; j++)
+        {
+            var max = 0;
+            for (var i = 0; i < l; i++)
+            {
+                if (grid[i][j] > max)
+                {
+                    max = grid[i][j];
+                }
+            }
+
+            y += max;
+        }
+
+        return x + y + z;
+    }
+}
