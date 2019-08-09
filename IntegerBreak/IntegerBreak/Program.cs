b14b7dc897215a63bf2a8cf725bbfe1d6ebfa33a
Timur Koshel <t.koshel@come2play.com>
2019-08-09 09:25:35 +0300

    Integer Break

---
+++ IntegerBreak/IntegerBreak/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IntegerBreak
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().IntegerBreak(2));
+            Console.WriteLine(36 == new Solution().IntegerBreak(10));
+        }
+    }
+}
+
+public class Solution {
+    public int IntegerBreak(int n) {
+        if (n == 2)
+        {
+            return 1;
+        }
+
+        if (n == 3)
+        {
+            return 2;
+        }
+
+        var product = 1;
+
+        while (n > 4)
+        {
+            product *= 3;
+            n -= 3;
+        }
+
+        product *= n;
+
+        return product;
+    }
+}
