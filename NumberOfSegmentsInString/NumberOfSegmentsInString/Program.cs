522754c386020df7cc54c1501c386276086606ed
Timur Koshel <t.koshel@come2play.com>
2019-08-15 09:07:39 +0300

    Number Of Segments In String

---
+++ NumberOfSegmentsInString/NumberOfSegmentsInString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumberOfSegmentsInString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().CountSegments("Hello, my name is John"));
+            Console.WriteLine(0 == new Solution().CountSegments(""));
+            Console.WriteLine(0 == new Solution().CountSegments("                "));
+        }
+    }
+}
+
+public class Solution {
+    public int CountSegments(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        var split = s.Split(' ');
+
+        var count = 0;
+        
+        foreach (var c in split)
+        {
+            if (string.IsNullOrEmpty(c))
+            {
+                continue;
+            }
+
+            count++;
+        }
+
+        return count;
+    }   
+}
