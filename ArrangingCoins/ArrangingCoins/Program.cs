da6ea2d4105074a5cf86ce1c7913f92838c96ed3
Timur Koshel <t.koshel@come2play.com>
2019-08-20 09:57:20 +0300

    Arranging Coins

---
+++ ArrangingCoins/ArrangingCoins/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrangingCoins
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(0 == new Solution().ArrangeCoins(0));
+            Console.WriteLine(1 == new Solution().ArrangeCoins(1));
+            Console.WriteLine(2 == new Solution().ArrangeCoins(3));
+            Console.WriteLine(2 == new Solution().ArrangeCoins(5));
+            Console.WriteLine(3 == new Solution().ArrangeCoins(6));
+            Console.WriteLine(3 == new Solution().ArrangeCoins(8));
+            Console.WriteLine(4 == new Solution().ArrangeCoins(10));
+        }
+    }
+}
+
+public class Solution {
+    public int ArrangeCoins(int n)
+    {
+        var result = 0;
+        var count = 1;
+
+        while (n - count >= 0)
+        {
+            n -= count;
+            count++;
+            result++;
+        }
+        
+        return result;
+    }
+}
