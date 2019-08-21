70e0586e18e92ce966c9e27754943734e5f7fef9
Timur Koshel <t.koshel@come2play.com>
2019-08-21 09:29:36 +0300

    String Compressino

---
+++ StringCompression/StringCompression/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StringCompression
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(6 == new Solution().Compress(new[] {'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+            Console.WriteLine(1 == new Solution().Compress(new[] {'a'}));
+            Console.WriteLine(4 == new Solution().Compress(new[] {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' }));
+        }
+    }
+}
+
+public class Solution
+{
+    public int Compress(char[] chars)
+    {
+        var anchor = 0;
+        var write = 0;
+
+        for (var read = 0; read < chars.Length; read++)
+        {
+            if (read + 1 == chars.Length || chars[read + 1] != chars[read])
+            {
+                chars[write++] = chars[anchor];
+                
+                if (read > anchor)
+                {
+                    foreach (var c in (read - anchor + 1).ToString())
+                    {
+                        chars[write++] = c;
+                    }
+                }
+                
+                anchor = read + 1;
+            }
+        }
+
+        return write;
+    }
+}
