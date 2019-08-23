a25aaaddb76d48dc52130365c5dd015d9181b515
Timur Koshel <t.koshel@come2play.com>
2019-08-23 10:41:21 +0300

    Find adll numbers disappeared in an array

---
+++ FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAllNumbersDisappearedInAnArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().FindDisappearedNumbers(new[] {4, 3, 2, 7, 8, 2, 3, 1});
+            Console.WriteLine(string.Join(",", result));
+            
+            var result2 = new Solution().FindDisappearedNumbers(new[] {1, 1});
+            Console.WriteLine(string.Join(",", result2));
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        var result = new List<int>();
+        var set = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            set.Add(num);
+        }
+
+        for (var i = 1; i <= nums.Length; i++)
+        {
+            if (set.Contains(i))
+            {
+                continue;
+            }
+            
+            result.Add(i);
+        }
+
+        return result;
+    }
+}
