aa58655e2c8968c8a3522af09ee5bbabe0b2242a
Timur Koshel <t.koshel@come2play.com>
2019-08-23 11:29:14 +0300

    Min moves to equal array elements

---
+++ MinMovesToEqualArrayElemnts/MinMovesToEqualArrayElemnts/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MinMovesToEqualArrayElemnts
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().MinMoves(new[] {1, 2, 3}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MinMoves(int[] nums)
+    {
+        if (nums.Length < 2)
+        {
+            return 0;
+        }
+
+        var min = int.MaxValue;
+        var sum = 0;
+
+        foreach (var num in nums)
+        {
+            sum += num;
+            min = Math.Min(min, num);
+        }
+
+        return sum - min * nums.Length;
+    }
+}
