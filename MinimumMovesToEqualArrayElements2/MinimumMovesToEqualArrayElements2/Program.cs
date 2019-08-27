725bda53fea1b5255909ceed691a90a92122a6cd
Timur Koshel <t.koshel@come2play.com>
2019-08-27 10:53:10 +0300

    Minimum Moves To Equal Array Elements 2

---
+++ MinimumMovesToEqualArrayElements2/MinimumMovesToEqualArrayElements2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MinimumMovesToEqualArrayElements2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(2 == new Solution().MinMoves2(new []{ 1, 2, 3}));
+        }
+    }
+}
+
+public class Solution {
+    public int MinMoves2(int[] nums)
+    {
+        var sum = 0;
+        var median = QuickSelect(nums, nums.Length / 2 + 1, 0, nums.Length - 1);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += Math.Abs(nums[i] - median);
+        }
+
+        return sum;
+    }
+
+    private int QuickSelect(int[] nums, int k, int start, int end)
+    {
+        var l = start;
+        var r = end;
+        var pivot = nums[(l + r) / 2];
+
+        while (l <= r)
+        {
+            while (nums[l] < pivot)
+            {
+                l++;
+            }
+
+            while (nums[r] > pivot)
+            {
+                r--;
+            }
+            
+            if (l >= r)
+            {
+                break;
+            }
+
+            Swap(nums, l++, r--);
+        }
+
+        if (l - start + 1 > k)
+        {
+            return QuickSelect(nums, k, start, l - 1);
+        }
+
+        if (l - start + 1 == k && l == r)
+        {
+            return nums[l];
+        }
+
+        return QuickSelect(nums, k - r + start - 1, r + 1, end);
+    }
+
+    private void Swap(int[] nums, int i, int j)
+    {
+        var tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
---
+++ MinimumMovesToEqualArrayElements2/MinimumMovesToEqualArrayElements2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MinimumMovesToEqualArrayElements2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinimumMovesToEqualArrayElements2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D0BF346E-A7E9-45C7-9F85-6C541B3C0574")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
