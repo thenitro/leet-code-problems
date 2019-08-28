db479cc0fb54a3dbb2f377bde16984d9c00aa0ea
Timur Koshel <t.koshel@come2play.com>
2019-08-28 11:32:59 +0300

    Assign Cookies

---
+++ AssignCookies/AssignCookies/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssignCookies
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().FindContentChildren(new[] {1, 2, 3}, new[] {1, 1}));
+            Console.WriteLine(2 == new Solution().FindContentChildren(new[] {1, 2}, new[] {1, 2, 3}));
+            Console.WriteLine(1 == new Solution().FindContentChildren(new[] {1, 2, 3}, new[] {3}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        Array.Sort(g);
+        Array.Sort(s);
+
+        var i = 0;
+
+        for (int j = 0; i < g.Length && j < s.Length; j++)
+        {
+            if (g[i] <= s[j])
+            {
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
---
+++ AssignCookies/AssignCookies/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AssignCookies")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssignCookies")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D0A391F3-9738-4BEC-87F3-A0A5C09584F7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
