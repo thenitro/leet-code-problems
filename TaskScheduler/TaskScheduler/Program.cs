9bb3fd6880c4647eb1d754046b94d0a95eb86f0b
Timur Koshel <t.koshel@come2play.com>
2019-08-29 10:22:33 +0300

    Task Scheduler

---
+++ TaskScheduler/TaskScheduler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskScheduler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(8 == new Solution().LeastInterval(new []{ 'A', 'A', 'A', 'B', 'B', 'B' }, 2));
+            Console.WriteLine(6 == new Solution().LeastInterval(new []{ 'A', 'A', 'A', 'B', 'B', 'B' }, 0));
+            Console.WriteLine(104 == new Solution().LeastInterval(new []{ 'A', 'A', 'A', 'B', 'B', 'B' }, 50));
+        }
+    }
+}
+
+public class Solution {
+    public int LeastInterval(char[] tasks, int n)
+    {
+        var map = new int[26];
+
+        foreach (var c in tasks)
+        {
+            map[c - 'A']++;
+        }
+        
+        Array.Sort(map);
+
+        var maxVal = map[25] - 1;
+        var idleSlots = maxVal * n;
+
+        for (int i = 24; i >= 0; i--)
+        {
+            idleSlots -= Math.Min(map[i], maxVal);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.Length : tasks.Length;
+    }
+}
