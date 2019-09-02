8cbcfee1460a03366990492b9a8345fb7447ce66
Timur Koshel <t.koshel@come2play.com>
2019-09-02 09:01:21 +0300

    Best time to buy and sell stock with transaction fee

---
+++ BestTimeToBuyAndSellStockWithTransactionFee/BestTimeToBuyAndSellStockWithTransactionFee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStockWithTransactionFee
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(8 == new Solution().MaxProfit(new[] {1, 3, 2, 8, 4, 9}, 2));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MaxProfit(int[] prices, int fee)
+    {
+        var sell = 0;
+        var buy = -prices[0];
+        var maxSell = 0;
+
+        for (var i = 1; i < prices.Length; i++)
+        {
+            sell = Math.Max(sell, buy + prices[i] - fee);
+            buy = Math.Max(buy, sell - prices[i]);
+            maxSell = Math.Max(sell, maxSell);
+        }
+
+        return maxSell;
+    }
+}
