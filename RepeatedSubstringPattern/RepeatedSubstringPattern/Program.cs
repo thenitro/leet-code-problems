8be437cfb04a8c3d4cc0a67614db2990f3834bb8
Timur Koshel <t.koshel@come2play.com>
2019-09-04 09:05:35 +0300

    repeated substring pattern

---
+++ RepeatedSubstringPattern/RepeatedSubstringPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatedSubstringPattern
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().RepeatedSubstringPattern("bb"));
+            Console.WriteLine(true == new Solution().RepeatedSubstringPattern("abab"));
+            Console.WriteLine(false == new Solution().RepeatedSubstringPattern("abac"));
+            Console.WriteLine(false == new Solution().RepeatedSubstringPattern("aba"));
+            Console.WriteLine(true == new Solution().RepeatedSubstringPattern("abcabcabcabc"));
+        }
+    }
+}
+
+public class Solution
+ {
+     public bool RepeatedSubstringPattern(string s)
+     {
+         for (var i = s.Length / 2; i >= 1; i--)
+         {
+             if (s.Length % i != 0)
+             {
+                 continue;
+             }
+ 
+             var m = s.Length / i;
+             var subS = s.Substring(0, i);
+             var sb = new StringBuilder();
+             
+             for (var j = 0; j < m; j++)
+             {
+                 sb.Append(subS);
+             }
+ 
+             if (sb.ToString().Equals(s))
+             {
+                 return true;
+             }
+         }
+ 
+         return false;
+     }
+ }
