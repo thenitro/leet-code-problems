31869b69207266fa67e4487e66918c5020c47650
Timur Koshel <t.koshel@come2play.com>
2019-09-05 09:34:20 +0300

    Hamming distance

---
+++ HammingDistance/HammingDistance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HammingDistance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().HammingDistance(1, 4));
+        }
+    }
+}
+
+public class Solution {
+    public int HammingDistance(int x, int y)
+    {
+        var strX = Convert.ToString(x, 2);
+        var strY = Convert.ToString(y, 2);
+
+        var smaller = Math.Min(strX.Length, strY.Length);
+        var bigger = Math.Max(strX.Length, strY.Length);
+
+        var count = 0;
+
+        for (var i = 0; i < bigger; i++)
+        {
+            var xIndex = strX.Length - i - 1;
+            var yIndex = strY.Length - i - 1;
+
+            var xValue = 0;
+            if (xIndex >= 0)
+            {
+                xValue = strX[xIndex] - '0';
+            }
+            
+            var yValue = 0;
+            if (yIndex >= 0)
+            {
+                yValue = strY[yIndex] - '0';
+            }
+            
+            if (xValue != yValue)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
