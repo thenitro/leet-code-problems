eda1a2bec3f6f32249a8f51d76faeb9eaa2ad0a5
Timur Koshel <t.koshel@come2play.com>
2019-09-16 11:51:31 +0300

    License key formatting

---
+++ LicenseKeyFormatting/LicenseKeyFormatting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LicenseKeyFormatting
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("5F3Z-2E9W" == new Solution().LicenseKeyFormatting("5F3Z-2e-9-w", 4));
+            Console.WriteLine("2-5G-3J" == new Solution().LicenseKeyFormatting("2-5g-3-J", 2));
+        }
+    }
+}
+
+public class Solution
+{
+    public string LicenseKeyFormatting(string S, int K)
+    {
+        const string delimiter = "-";
+        var sb = new StringBuilder();
+
+        for (var i = S.Length - 1; i >= 0; i--)
+        {
+            if (S[i] != '-')
+            {
+                sb.Append(sb.Length % (K + 1) == K ? delimiter : string.Empty).Append(S[i]);
+            }
+        }
+
+        return new string(sb.ToString().ToUpper().Reverse().ToArray());
+    }
+}
