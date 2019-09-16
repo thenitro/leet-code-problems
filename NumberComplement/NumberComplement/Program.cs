9078d420881cd7831bb40eaac3b385cb2bbde480
Timur Koshel <t.koshel@come2play.com>
2019-09-16 10:42:34 +0300

    Number complement

---
+++ NumberComplement/NumberComplement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberComplement
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().FindComplement(5));
+            Console.WriteLine(0 == new Solution().FindComplement(1));
+        }
+    }
+}
+
+public class Solution {
+    public int FindComplement(int num)
+    {
+        var str = Convert.ToString(num, 2);
+        var val = string.Empty;
+        
+        for (var i = 0; i < str.Length; i++)
+        {
+            val += str[i] == '1' ? '0' : '1';
+        }
+        
+        return Convert.ToInt32(val, 2);
+    }
+}
