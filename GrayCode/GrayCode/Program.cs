264bbe6d0d5fe7843ed358575b75712e3432c98f
Timur Koshel <t.koshel@come2play.com>
2019-09-17 14:16:00 +0300

    GrayCode

---
+++ GrayCode/GrayCode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GrayCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result1 = new Solution().GrayCode(2);
+            Console.WriteLine(string.Join(",", result1));
+
+            var result2 = new Solution().GrayCode(0);
+            Console.WriteLine(string.Join(",", result2));
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<int> GrayCode(int n)
+    {
+        var result = new List<int>();
+
+        for (var i = 0; i < 1 << n; i++)
+        {
+            result.Add(i ^ i >> 1);
+        }
+        
+        return result;
+    }
+}
