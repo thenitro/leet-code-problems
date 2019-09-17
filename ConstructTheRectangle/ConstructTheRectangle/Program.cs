ae6447d2cc8cab33e926b474048facbe41fb6d07
Timur Koshel <t.koshel@come2play.com>
2019-09-17 09:42:18 +0300

    Construct the rectangle

---
+++ ConstructTheRectangle/ConstructTheRectangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConstructTheRectangle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result1 = new Solution().ConstructRectangle(1);
+            Console.WriteLine("1,1" == string.Join(",", result1));
+            
+            var result3 = new Solution().ConstructRectangle(2);
+            Console.WriteLine("2,1" == string.Join(",", result3));
+            
+            var result = new Solution().ConstructRectangle(4);
+            Console.WriteLine("2,2" == string.Join(",", result));
+            
+            var result2 = new Solution().ConstructRectangle(6);
+            Console.WriteLine("3,2" == string.Join(",", result2));
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] ConstructRectangle(int area)
+    {
+        var sqrt = (int)Math.Sqrt(area);
+
+        while (area % sqrt != 0)
+        {
+            sqrt--;
+        }
+
+        return new[] {area / sqrt, sqrt};
+    }
+}
