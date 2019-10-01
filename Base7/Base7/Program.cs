e8e3be63aaf11c49d5966ad0cb57c68810ca5145
Timur Koshel <t.koshel@come2play.com>
2019-10-01 11:20:54 +0300

    Base 7

---
+++ Base7/Base7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Base7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("202" == new Solution().ConvertToBase7(100));
+            Console.WriteLine("-10" == new Solution().ConvertToBase7(-7));
+        }
+    }
+}
+
+public class Solution
+{
+    public string ConvertToBase7(int num)
+    {
+        if (num < 0)
+        {
+            return '-' + ConvertToBase7(-num);
+        }
+
+        if (num < 7)
+        {
+            return num + "";
+        }
+
+        return ConvertToBase7(num / 7) + num % 7;
+    }
+}
