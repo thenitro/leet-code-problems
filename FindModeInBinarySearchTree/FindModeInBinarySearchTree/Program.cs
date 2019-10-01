58023cf8b62274da9a1fd39f4d4d9b74a8276214
Timur Koshel <t.koshel@come2play.com>
2019-10-01 11:13:16 +0300

    Find mode in binary search tree, Keyboard row

---
+++ FindModeInBinarySearchTree/FindModeInBinarySearchTree/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindModeInBinarySearchTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().FindMode(new TreeNode(1)
+            {
+                right = new TreeNode(2)
+                {
+                    left = new TreeNode(2)
+                }
+            });
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x)
+    {
+        val = x;
+    }
+}
+
+public class Solution
+{
+    public int[] FindMode(TreeNode root)
+    {
+        var result = new List<int>();
+        var dict = new Dictionary<int, int>();
+
+        var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+        var maxAmount = int.MinValue;
+            
+        while (queue.Count > 0)
+        {
+            var next = queue.Dequeue();
+            if (next == null)
+            {
+                continue;
+            }
+
+            dict[next.val] = dict.ContainsKey(next.val) ? dict[next.val] + 1 : 1;
+
+            if (dict[next.val] > maxAmount)
+            {
+                maxAmount = dict[next.val];
+            }
+            
+            queue.Enqueue(next.left);
+            queue.Enqueue(next.right);
+        }
+
+        foreach (var kv in dict)
+        {
+            if (kv.Value == maxAmount)
+            {
+                result.Add(kv.Key);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ KeyboardRow/KeyboardRow/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyboardRow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().FindWords(new []{ "Hello", "Alaska", "Dad", "Peace" });
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+}
+
+public class Solution
+{
+    private HashSet<char> Row1 = new HashSet<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+    private HashSet<char> Row2 = new HashSet<char>() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+    private HashSet<char> Row3 = new HashSet<char>() { 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+    
+    public string[] FindWords(string[] words) 
+    {
+        var result = new List<string>();
+        
+        foreach (var word in words)
+        {
+            var hasFromRow1 = false;
+            var hasFromRow2 = false;
+            var hasFromRow3 = false;
+
+            foreach (var source in word)
+            {
+                var symbol = Char.ToLower(source);
+                
+                if (!hasFromRow1)
+                {
+                    hasFromRow1 = Row1.Contains(symbol);
+                }
+                
+                if (!hasFromRow2)
+                {
+                    hasFromRow2 = Row2.Contains(symbol);
+                }
+                
+                if (!hasFromRow3)
+                {
+                    hasFromRow3 = Row3.Contains(symbol);
+                }
+            }
+
+            var amountOfRows = 0;
+                amountOfRows += hasFromRow1 ? 1 : 0;
+                amountOfRows += hasFromRow2 ? 1 : 0;
+                amountOfRows += hasFromRow3 ? 1 : 0;
+
+            if (amountOfRows == 1)
+            {
+                result.Add(word);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
