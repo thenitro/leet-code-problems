11d36fe0dfa56ecfe515f059f63eae28b4da063c
Timur Koshel <t.koshel@come2play.com>
2019-10-02 10:00:21 +0300

    Number of music playlists, relative ranks

---
+++ NumberOfMusicPlaylists/NumberOfMusicPlaylists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberOfMusicPlaylists
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(6 == new Solution().NumMusicPlaylists(3, 3, 1));
+            Console.WriteLine(6 == new Solution().NumMusicPlaylists(2, 3, 0));
+            Console.WriteLine(2 == new Solution().NumMusicPlaylists(2, 3, 1));
+        }
+    }
+}
+
+public class Solution
+{
+    public int NumMusicPlaylists(int N, int L, int K)
+    {
+        var mod = 1_000_000_007;
+        
+        var dp = new long[L+1, N+1];
+        dp[0, 0] = 1;
+
+        for (int i = 1; i <= L; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                dp[i, j] += dp[i - 1, j - 1] * (N - j + 1);
+                dp[i, j] += dp[i - 1, j] * Math.Max(j - K, 0);
+                dp[i, j] %= mod;
+            }
+        }
+
+        return (int) dp[L, N];
+    }
+}
---
+++ RelativeRanks/RelativeRanks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RelativeRanks
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().FindRelativeRanks(new[] {5, 4, 3, 2, 1});
+            Console.WriteLine(string.Join(",", result));//1 2 3
+            
+            var result2 = new Solution().FindRelativeRanks(new[] {10, 3, 8, 9, 4});
+            Console.WriteLine(string.Join(",", result2)); // 1, 5, 3, 2
+        }
+    }
+}
+
+public class Solution
+{
+    public string[] FindRelativeRanks(int[] nums)
+    {
+        var sorted = new int[nums.Length];
+        
+        Array.Copy(nums, sorted, nums.Length);
+        Array.Sort(sorted);
+        Array.Reverse(sorted);
+        
+        var result = new string[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var index = Array.IndexOf(sorted, nums[i]);
+            
+            if (index == 0)
+            {
+                result[i] = "Gold Medal";
+            }
+            else if (index == 1)
+            {
+                result[i] = "Silver Medal";
+            }
+            else if (index == 2)
+            {
+                result[i] = "Bronze Medal";
+            }
+            else
+            {
+                result[i] = (index + 1).ToString();
+            }
+        }
+
+        return result;
+    }
+}
