83aebb2f4bb6728c8ca5afb5ed9b1dc3192fe614
Timur Koshel <t.koshel@come2play.com>
2019-10-03 14:51:46 +0300

    Detect capital

---
+++ DetectCapital/DetectCapital/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DetectCapital
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().DetectCapitalUse("USA"));
+            Console.WriteLine(true == new Solution().DetectCapitalUse("leetcode"));
+            Console.WriteLine(true == new Solution().DetectCapitalUse("Google"));
+            Console.WriteLine(false == new Solution().DetectCapitalUse("FlaG"));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        var amountOfCapitals = CalculateCapitalsAmount(word);
+        
+        return (FirstLetterIsCapital(word) && amountOfCapitals == 1) || amountOfCapitals == 0 || amountOfCapitals == word.Length;
+    }
+
+    private int CalculateCapitalsAmount(string word)
+    {
+        var count = 0;
+        
+        foreach (var character in word)
+        {
+            if (Char.IsUpper(character))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private bool FirstLetterIsCapital(string word)
+    {
+        return Char.IsUpper(word[0]);
+    }
+}
