421880de0a4076e0ccae02c972666d04c9e0bdb6
Timur Koshel <t.koshel@come2play.com>
2019-10-03 14:13:20 +0300

    Fibonacci number

---
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FibonacciNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().Fib(2));
+            Console.WriteLine(2 == new Solution().Fib(3));
+            Console.WriteLine(3 == new Solution().Fib(4));
+        }
+    }
+}
+
+public class Solution
+{
+    public int Fib(int N)
+    {
+        if (N == 0 || N == 1)
+        {
+            return N;
+        }
+        
+        var memo = new int[N + 1];
+            memo[0] = 0;
+            memo[1] = 1;
+
+        for (var i = 2; i <= N; i++)
+        {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        
+        return memo[N];
+    }
+}
