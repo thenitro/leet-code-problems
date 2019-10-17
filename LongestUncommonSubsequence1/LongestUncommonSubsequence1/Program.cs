467579fcb937c00aef79c2de65b4b239ccf49579
Timur Koshel <t.koshel@come2play.com>
2019-10-17 10:07:53 +0300

    Longest Uncommon Subsequence 1

---
+++ LongestUncommonSubsequence1/LongestUncommonSubsequence1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LongestUncommonSubsequence1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().FindLUSlength("aba", "cdc"));
+            Console.WriteLine(3 == new Solution().FindLUSlength("a", "adc"));
+            Console.WriteLine(7 == new Solution().FindLUSlength("aba", "cabaedc"));
+            Console.WriteLine(7 == new Solution().FindLUSlength("eaba", "cabaedc"));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindLUSlength(string a, string b)
+    {
+        if (a.Equals(b))
+        {
+            return -1;
+        }
+
+        return Math.Max(a.Length, b.Length);
+    }
+}
