2580b9fc99133da4e3aae8ba943b55f351b56ab6
Timur Koshel <t.koshel@come2play.com>
2019-11-07 10:01:52 +0200

    Diameter of binary tree and Num Trees

---
+++ DiameterOfBinaryTree/DiameterOfBinaryTree/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DiameterOfBinaryTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+            };
+            Console.WriteLine(3 == new Solution().DiameterOfBinaryTree(root));
+
+            var root1 = new TreeNode(1);
+            Console.WriteLine(0 == new Solution().DiameterOfBinaryTree(root1));
+            
+            Console.WriteLine(0 == new Solution().DiameterOfBinaryTree(null));
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution
+{
+    private int _result;
+    
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        _result = 1;
+
+        DiameterOfBinaryTreeHelper(root);
+        
+        return _result - 1;
+    }
+
+    private int DiameterOfBinaryTreeHelper(TreeNode node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        var l = DiameterOfBinaryTreeHelper(node.left);
+        var r = DiameterOfBinaryTreeHelper(node.right);
+
+        _result = Math.Max(_result, l + r + 1);
+
+        return Math.Max(l, r) + 1;
+    }
+}
---
+++ NumTrees/NumTrees/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumTrees
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().NumTrees(3));
+        }
+    }
+}
+
+public class Solution
+{
+    public int NumTrees(int n)
+    {
+        var G = new int[n + 1];
+            G[0] = 1;
+            G[1] = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            for (var j = 1; j <= i; j++)
+            {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+
+        return G[n];
+    }
+}
