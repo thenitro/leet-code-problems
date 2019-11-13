973b3cc2c8213faaba21e50564bfaded480d27ab
Timur Koshel <t.koshel@come2play.com>
2019-11-13 14:51:41 +0200

    Student attendance record 1

---
+++ StudentAttendanceRecord1/StudentAttendanceRecord1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace StudentAttendanceRecord1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().CheckRecord("PPALLP"));
+            Console.WriteLine(false == new Solution().CheckRecord("PPALLL"));
+            Console.WriteLine(true == new Solution().CheckRecord("LLAPPP"));
+            Console.WriteLine(false == new Solution().CheckRecord("LLLAPP"));
+            Console.WriteLine(false == new Solution().CheckRecord("LLAPPA"));
+        }
+    }
+}
+
+public class Solution {
+    public bool CheckRecord(string s)
+    {
+        var amountOfAs = 0;
+        var amountOfContinuosL = 0;
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'A')
+            {
+                amountOfAs++;
+
+                if (amountOfAs > 1)
+                {
+                    return false;
+                }
+            }
+
+            if (s[i] == 'L')
+            {
+                amountOfContinuosL++;
+
+                if (amountOfContinuosL > 2)
+                {
+                    return false;
+                }
+            } 
+            else
+            {
+                amountOfContinuosL = 0;
+            }
+        }
+
+        return true;
+    }
+}
