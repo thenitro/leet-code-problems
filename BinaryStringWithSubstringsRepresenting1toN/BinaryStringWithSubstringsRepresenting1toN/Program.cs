4134c1a916bb70933930f0eb0381788dc9f5c5e9
Timur Koshel <t.koshel@come2play.com>
2019-12-13 10:49:30 +0200

    Binary string with substrings representing 1 to N

---
+++ BinaryStringWithSubstringsRepresenting1toN/BinaryStringWithSubstringsRepresenting1toN/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BinaryStringWithSubstringsRepresenting1toN
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().QueryString("0110", 3));
+            Console.WriteLine(false == new Solution().QueryString("0110", 4));
+        }
+    }
+}
+
+public class Solution {
+    public bool QueryString(string S, int N)
+    {
+        var result = true;
+        
+        for (var i = 1; i <= N; i++)
+        {
+            var binary = Convert.ToString(i, 2);
+            
+            if (!S.Contains(binary))
+            {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
