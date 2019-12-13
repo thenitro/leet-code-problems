0f2f4a54ec06000e2e86c07c10488cba601e9cf0
Timur Koshel <t.koshel@come2play.com>
2019-12-13 15:17:59 +0200

    Shortest unsorted continuous subarray

---
+++ ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace ShortestUnsortedContinuousSubarray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().FindUnsortedSubarray(new[] {2, 6, 4, 8, 10, 9, 15}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindUnsortedSubarray(int[] nums)
+    {
+        var length = nums.Length;
+        var sortNums = new int[length];
+        
+        Array.Copy(nums, sortNums, length);
+        Array.Sort(sortNums);
+
+        var start = length;
+        var end = 0;
+
+        for (var i = 0; i < length; i++)
+        {
+            if (sortNums[i] != nums[i])
+            {
+                start = Math.Min(start, i);
+                end = Math.Max(end, i);
+            }
+        }
+
+        return end - start >= 0 ? end - start + 1 : 0;
+    }
+}
