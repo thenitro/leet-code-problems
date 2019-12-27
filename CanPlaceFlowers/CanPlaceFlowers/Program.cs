f0fdbcfb024442abd371d12920040e9fc57aca0a
Timur Koshel <timurkoshel@gmail.com>
2019-12-27 21:37:07 +0200

    Convert Bst To Greater Tree, Can Place Flowers

---
+++ CanPlaceFlowers/CanPlaceFlowers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CanPlaceFlowers
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().CanPlaceFlowers(new[] {1, 0, 0, 0, 1}, 1));
+            Console.WriteLine(false == new Solution().CanPlaceFlowers(new[] {1, 0, 0, 0, 1}, 2));
+            Console.WriteLine(false == new Solution().CanPlaceFlowers(new[] {1, 0, 0, 0, 0, 1}, 2));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        var result = 0;
+        
+        for (var i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 1)
+            {
+                continue;
+            }
+            
+            var rightIsFree = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0; 
+            var leftIsFree = i == 0 || flowerbed[i - 1] == 0; 
+            
+            if (leftIsFree && rightIsFree)
+            {
+                flowerbed[i] = 1;
+                result++;
+            }
+        }
+
+        return result >= n;
+    }
+}
---
+++ ConvertBstToGreaterTree/ConvertBstToGreaterTree/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ConvertBstToGreaterTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new TreeNode(5)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(13)
+            };
+
+            new Solution().ConvertBST(tree);
+
+            InOrderPrint(tree);
+        }
+
+        private static void InOrderPrint(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            
+            InOrderPrint(root.left);
+            Console.Write(root.val + " ");
+            InOrderPrint(root.right);
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution
+{
+    private int _currentValue = 0;
+    
+    public TreeNode ConvertBST(TreeNode root)
+    {
+        InOrderHelper(root);
+        return root;
+    }
+
+    private void InOrderHelper(TreeNode root)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        
+        InOrderHelper(root.right);
+        
+        var newValue = _currentValue + root.val;
+        root.val = newValue;
+        _currentValue = newValue;
+        
+        InOrderHelper(root.left);
+    }
+}
