eb38bf18820646a40d18f5cadadf8a9cd971bced
Timur Koshel <t.koshel@come2play.com>
2020-01-13 14:02:48 +0200

    Top K Frequent elements

---
+++ TopKFrequentElements/TopKFrequentElements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TopKFrequentElements
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", new Solution().TopKFrequent(new []{ 1, 1, 1, 2, 2, 3}, 2)));
+            Console.WriteLine(string.Join(",", new Solution().TopKFrequent(new []{ 1 }, 1)));
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<int> TopKFrequent(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            dict[num] = dict.ContainsKey(num) ? dict[num] + 1 : 1;
+        }
+
+        var result = new List<int>();
+        
+        for (var i = 0; i < k; i++)
+        {
+            var max = int.MinValue;
+            var item = 0;
+            
+            foreach (var kv in dict)
+            {
+                if (kv.Value > max)
+                {
+                    max = kv.Value;
+                    item = kv.Key;
+                }
+            }
+
+            dict.Remove(item);
+            result.Add(item);
+        }
+
+        return result;
+    }
+}
