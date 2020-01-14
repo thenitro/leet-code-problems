f3f2945bbee6d1a61c537e21ac11c771938609e2
Timur Koshel <t.koshel@come2play.com>
2020-01-14 10:13:13 +0200

    Find The Duplicate Number

---
+++ FindTheDuplicateNumber/FindTheDuplicateNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FindTheDuplicateNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().FindDuplicate(new[] {1, 3, 4, 2, 2}));
+            Console.WriteLine(3 == new Solution().FindDuplicate(new[] {3, 1, 3, 4, 2}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        var tortoise = nums[0];
+        var hare = nums[0];
+
+        do
+        {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+        
+        var ptr1 = nums[0];
+        var ptr2 = tortoise;
+
+        while (ptr1 != ptr2)
+        {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+}
