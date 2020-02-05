a28b4bffdb47b20e232fab7029ecf07c3db56723
Timur Koshel <t.koshel@come2play.com>
2020-02-05 11:07:58 +0200

    Target Sum

---
+++ TargetSum/TargetSum/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TargetSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().FindTargetSumWays(new int[] {1, 1, 1, 1, 1}, 3));
+        }
+    }
+}
+
+public class Solution
+{
+    private int _result = 0;
+    
+    public int FindTargetSumWays(int[] nums, int S)
+    {
+        var result = new int[(int)Math.Pow(2, nums.Length - 1)];
+        
+        _result = 0;
+        
+        Dfs(nums, 0, 0, S);
+
+        return _result;
+    }
+
+    private void Dfs(int[] nums, int startIndex, int currentSum, int targetSum)
+    {
+        if (startIndex >= nums.Length)
+        {
+            if (currentSum == targetSum)
+            {
+                _result++;
+            }
+            
+            return;
+        }
+
+        Dfs(nums, startIndex + 1, currentSum + nums[startIndex], targetSum);
+        Dfs(nums, startIndex + 1, currentSum - nums[startIndex], targetSum);
+    }
+}
+
+public class SolutionDfs
+{
+    private int _result = 0;
+    
+    public int FindTargetSumWays(int[] nums, int S)
+    {
+        _result = 0;
+        
+        Dfs(nums, 0, 0, S);
+
+        return _result;
+    }
+
+    private void Dfs(int[] nums, int startIndex, int currentSum, int targetSum)
+    {
+        if (startIndex >= nums.Length)
+        {
+            if (currentSum == targetSum)
+            {
+                _result++;
+            }
+            
+            return;
+        }
+
+        Dfs(nums, startIndex + 1, currentSum + nums[startIndex], targetSum);
+        Dfs(nums, startIndex + 1, currentSum - nums[startIndex], targetSum);
+    }
+}
