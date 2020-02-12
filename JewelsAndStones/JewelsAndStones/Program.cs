b9e4cb2f201bec987084033def1d3c564745a587
Timur Koshel <t.koshel@come2play.com>
2020-02-12 13:40:31 +0200

    Jewels and Stones

---
+++ JewelsAndStones/JewelsAndStones/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelsAndStones
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().NumJewelsInStones("aA", "aAAbbbb"));
+            Console.WriteLine(0 == new Solution().NumJewelsInStones("z", "ZZ"));
+        }
+    }
+}
+
+public class Solution
+{
+    public int NumJewelsInStones(string J, string S)
+    {
+        var dictionary = new Dictionary<char, int>();
+
+        for (var i = 0; i < S.Length; i++)
+        {
+            var character = S[i];
+            
+            dictionary[character] = dictionary.ContainsKey(character) ? dictionary[character] + 1 : 1;
+        }
+
+        var result = 0;
+
+        for (var i = 0; i < J.Length; i++)
+        {
+            var character = J[i];
+
+            if (dictionary.ContainsKey(character))
+            {
+                result += dictionary[character];
+            }
+        }
+
+        return result;
+    }
+}
