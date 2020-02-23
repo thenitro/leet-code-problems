b6d169e5778295c066f73207b8e471913de3d6c9
Timur Koshel <timurkoshel@gmail.com>
2020-02-23 16:35:06 +0200

    Maximum Length of Pair chain

---
+++ MaximumLengthOfPairChain/MaximumLengthOfPairChain/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MaximumLengthOfPairChain
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().FindLongestChain(new[] {new[] {1, 2}, new[] {2, 3}, new[] {3, 4}}));
+            Console.WriteLine(2 == new Solution().FindLongestChain(new[] {new[] {3, 4}, new[] {2, 3}, new[] {1, 2}}));
+            Console.WriteLine(4 == new Solution().FindLongestChain(new[] {new[]{-10,-8},new[] {8,9},new[]{-5,0},new[]{6,10},new[]{-6,-4},new[]{1,7},new[]{9,10},new[]{-4,7}}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int FindLongestChain(int[][] pairs)
+    {
+        if (pairs == null || pairs.Length <= 1)
+        {
+            return 0;
+        } 
+        
+        Array.Sort(pairs, (a, b) => a[0] - b[0]);
+        
+        var N = pairs.Length;
+        var dp = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            dp[i] = 1;
+        }
+
+        for (var j = 1; j < N; j++)
+        {
+            for (var i = 0; i < j; i++)
+            {
+                if (pairs[i][1] < pairs[j][0])
+                {
+                    dp[j] = Math.Max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        var ans = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (dp[i] > ans)
+            {
+                ans = dp[i];
+            }
+        }
+        
+        return ans;
+    }
+}
---
+++ MaximumLengthOfPairChain/MaximumLengthOfPairChain/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaximumLengthOfPairChain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaximumLengthOfPairChain")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CD63BE33-9EBA-4565-97DE-62BBD851F301")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
