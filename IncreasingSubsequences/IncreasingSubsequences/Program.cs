6b5b43493bf3be2ce9eff3ada4cf207cdee837c1
Timur Koshel <t.koshel@come2play.com>
2020-02-24 14:05:39 +0200

    IncreasingSubsequences

---
+++ IncreasingSubsequences/IncreasingSubsequences/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IncreasingSubsequences
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Solution().FindSubsequences(new []{ 4, 6, 7, 7});
+
+            foreach (var subresult in result)
+            {
+                Console.WriteLine(string.Join(", ", subresult));
+            }
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<IList<int>> FindSubsequences(int[] nums)
+    {
+        var results = new List<IList<int>>();
+        Dfs(nums, 0, int.MinValue, new List<int>(), results);
+        return results;
+    }
+
+    private void Dfs(int[] nums, int startIndex, int prev, List<int> tmp, List<IList<int>> results)
+    {
+        if (tmp.Count > 1)
+        {
+            results.Add(new List<int>(tmp));
+        }
+
+        var used = new HashSet<int>();
+        
+        for (var i = startIndex; i < nums.Length; i++)
+        {
+            if (used.Contains(nums[i]) || prev > nums[i])
+            {
+                continue;
+            }
+            
+            tmp.Add(nums[i]);
+            used.Add(nums[i]);
+            Dfs(nums, i + 1, nums[i], tmp, results);
+            tmp.RemoveAt(tmp.Count - 1);
+        }
+    }
+}
