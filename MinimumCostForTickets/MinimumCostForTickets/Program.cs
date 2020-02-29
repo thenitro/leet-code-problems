6a873833b31baeb1456dc410166b184968a51a2d
Timur Koshel <timurkoshel@gmail.com>
2020-02-29 14:05:54 +0200

    Minimum cost for tickets

---
+++ MinimumCostForTickets/MinimumCostForTickets/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MinimumCostForTickets
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(11 == new Solution().MincostTickets(new[] {1, 4, 6, 7, 8, 20}, new[] {2, 7, 15}));
+            Console.WriteLine(17 == new Solution().MincostTickets(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31},new[] {2, 7, 15}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MincostTickets(int[] days, int[] costs)
+    {
+        const int DAYS = 366;
+        var travelDays = new bool[DAYS];
+
+        for (var i = 0; i < days.Length; i++)
+        {
+            travelDays[days[i]] = true;
+        }
+
+        var dp = new int[DAYS];
+
+        for (var i = 1; i < DAYS; i++)
+        {
+            dp[i] = int.MaxValue;
+        }
+
+        for (var i = 1; i < DAYS; i++)
+        {
+            if (!travelDays[i])
+            {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+            
+            dp[i] = Math.Min(dp[i], dp[i - 1] + costs[0]);
+            
+            if (i - 7 >= 0)
+            {
+                dp[i] = Math.Min(dp[i], dp[i - 7] + costs[1]);
+            }
+            else
+            {
+                dp[i] = Math.Min(dp[i], costs[1]);
+            }
+            
+            if (i - 30 >= 0)
+            {
+                dp[i] = Math.Min(dp[i], dp[i - 30] + costs[2]);
+            }
+            else
+            {
+                dp[i] = Math.Min(dp[i], costs[2]);
+            }
+        }
+        
+        return dp[DAYS - 1];
+    }
+}
---
+++ MinimumCostForTickets/MinimumCostForTickets/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MinimumCostForTickets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinimumCostForTickets")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("A93E6FF0-CB6B-4FA7-8C9B-BA24E9954FCA")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
