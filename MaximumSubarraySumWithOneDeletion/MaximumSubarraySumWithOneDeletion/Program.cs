c9d8eeaf455715e4b46b9ae0363e1a1eae6db8ec
Timur Koshel <timurkoshel@gmail.com>
2020-03-08 20:35:26 +0200

    Maximum subarray sum with one deletion

---
+++ MaximumSubarraySumWithOneDeletion/MaximumSubarraySumWithOneDeletion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MaximumSubarraySumWithOneDeletion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(4 == new Solution().MaximumSum(new[] {1, -2, 0, 3}));
+            Console.WriteLine(3 == new Solution().MaximumSum(new[] {1, -2, -2, 3}));
+            Console.WriteLine(-1 == new Solution().MaximumSum(new[] {-1, -1, -1, -1}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int MaximumSum(int[] arr)
+    {
+        var N = arr.Length;
+
+        var oneDelete = 0;
+        var noDelete = arr[0];
+
+        var maxSum = arr[0];
+
+        for (var i = 1; i < N; i++)
+        {
+            oneDelete = Math.Max(oneDelete + arr[i], noDelete);
+            noDelete = Math.Max(noDelete + arr[i], arr[i]);
+
+            maxSum = Math.Max(maxSum, Math.Max(oneDelete, noDelete));
+        }
+
+        return maxSum;
+    }
+}
---
+++ SubarraySumEqualsK/SubarraySumEqualsK/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SubarraySumEqualsK
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().SubarraySum(new[] {1, 1, 1}, 2));
+            Console.WriteLine(1 == new Solution().SubarraySum(new[] {28, 54, 7, -70, 22, 65, -6}, 100));
+            Console.WriteLine(55 == new Solution().SubarraySum(new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0));
+        }
+    }
+}
+
+public class Solution
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        var result = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var sum = 0;
+
+            for (var j = i; j < nums.Length; j++)
+            {
+                sum += nums[j];
+
+                if (sum == k)
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
