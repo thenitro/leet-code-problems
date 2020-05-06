8b3435eca875a9b7dc9b4394d3186de59a40a351
Timur Koshel <timurkoshel@gmail.com>
2020-05-06 22:12:45 +0300

    Replace Elements with Greatest Element on Right Side, Verifying Alien Dictionary

---
+++ ReplaceElementsWithGreatestElementOnRightSide/ReplaceElementsWithGreatestElementOnRightSide/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReplaceElementsWithGreatestElementOnRightSide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", new Solution().ReplaceElements(new []{ 17,18,5,4,6,1 })));
+            Console.WriteLine(string.Join(",", new Solution().ReplaceElements(new []{ 23727,37383,95658,71922,31756 })));
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        var mx = -1;
+        for (var i = arr.Length - 1; i >= 0; i--)
+        {
+            mx = Math.Max(arr[i], arr[i] = mx);
+        }
+
+        return arr;
+    }
+}
---
+++ VerifyingAlienDictionary/VerifyingAlienDictionary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerifyingAlienDictionary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().IsAlienSorted(new[] {"hello", "leetcode"}, "hlabcdefgijkmnopqrstuvwxyz"));
+            Console.WriteLine(false == new Solution().IsAlienSorted(new[] {"word", "world", "row"}, "worldabcefghijkmnpqstuvxyz"));
+            Console.WriteLine(false == new Solution().IsAlienSorted(new[] {"apple", "app"}, "abcdefghijklmnopqrstuvwxyz"));
+            Console.WriteLine(true == new Solution().IsAlienSorted(new[] {"kuvp", "q"}, "ngxlkthsjuoqcpavbfdermiywz"));
+            
+        }
+    }
+}
+
+public class Solution
+{
+    public bool IsAlienSorted(string[] words, string order)
+    {
+        var constructedOrder = new Dictionary<char, int>();
+
+        for (var i = 0; i < order.Length; i++)
+        {
+            constructedOrder[order[i]] = i;
+        }
+
+        for (var i = 0; i < words.Length - 1; i++)
+        {
+            var word1 = words[i];
+            var word2 = words[i + 1];
+
+            var length = Math.Min(word1.Length, word2.Length);
+
+            for (var k = 0; k < length; k++)
+            {
+                if (word1[k] != word2[k])
+                {
+                    if (constructedOrder[word1[k]] > constructedOrder[word2[k]])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        length = -1;
+                    }
+                }
+            }
+
+            if (length != -1 && word1.Length > word2.Length)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
