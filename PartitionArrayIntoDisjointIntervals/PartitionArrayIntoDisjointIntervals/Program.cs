673714cef13441e45af4861dcf905645559516f2
Timur Koshel <timurkoshel@gmail.com>
2020-05-10 09:39:16 +0300

    Partition array into disjoint intervals

---
+++ PartitionArrayIntoDisjointIntervals/PartitionArrayIntoDisjointIntervals/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PartitionArrayIntoDisjointIntervals
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().PartitionDisjoint(new[] {5, 0, 3, 8, 6}));
+            Console.WriteLine(4 == new Solution().PartitionDisjoint(new[] {1, 1, 1, 0, 6, 12}));
+            Console.WriteLine(1 == new Solution().PartitionDisjoint(new[] {1, 1}));
+            Console.WriteLine(1 == new Solution().PartitionDisjoint(new[] {26, 51, 40, 58, 42, 76, 30, 48, 79, 91}));
+        }
+    }
+}
+
+public class Solution
+{
+    public int PartitionDisjoint(int[] A)
+    {
+        var N = A.Length;
+
+        var maxLeft = new int[N];
+        var minRight = new int[N];
+
+        var m = A[0];
+        for (var i = 0; i < N; i++)
+        {
+            m = Math.Max(m, A[i]);
+            maxLeft[i] = m;
+        }
+        
+        m = A[N - 1];
+        for (var i = N - 1; i >= 0; i--)
+        {
+            m = Math.Min(m, A[i]);
+            minRight[i] = m;
+        }
+        
+        for (var i = 1; i < N; i++)
+        {
+            if (maxLeft[i - 1] <= minRight[i])
+            {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
---
+++ PartitionArrayIntoDisjointIntervals/PartitionArrayIntoDisjointIntervals/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PartitionArrayIntoDisjointIntervals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PartitionArrayIntoDisjointIntervals")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("E629FBF3-65DB-4C44-A2C1-454078073338")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
