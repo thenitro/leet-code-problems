0cd99a413283785df55ad4f3e72604e479427f3e
Timur Koshel <timurkoshel@gmail.com>
2020-05-17 16:02:24 +0300

    How many numers are smaller that the current number

---
+++ HowManyNumbersAreSmallerThanTheCurrentNumber/HowManyNumbersAreSmallerThanTheCurrentNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowManyNumbersAreSmallerThanTheCurrentNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", new Solution().SmallerNumbersThanCurrent(new[] {8, 1, 2, 2, 3})));
+            Console.WriteLine(string.Join(",", new Solution().SmallerNumbersThanCurrent(new[] {6, 5, 4, 8})));
+            Console.WriteLine(string.Join(",", new Solution().SmallerNumbersThanCurrent(new[] {7, 7, 7, 7})));
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        var N = nums.Length;
+        var sorted = new int[N];
+
+        Array.Copy(nums, sorted, N);
+        Array.Sort(sorted);
+
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < N; i++)
+        {
+            if (dict.ContainsKey(sorted[i]))
+            {
+                continue;
+            }
+
+            dict[sorted[i]] = i;
+        }
+
+        var result = new int[N];
+
+        for (var i = 0; i < N; i++)
+        {
+            result[i] = dict[nums[i]];
+        }
+
+        return result;
+    }
+}
