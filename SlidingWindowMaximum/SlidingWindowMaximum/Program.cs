1489fcb1984c9549d32fa25d0ea191313dc25ceb
Timur Koshel <timurkoshel@gmail.com>
2020-05-22 20:45:45 +0300

    Sliding Window Maximum, Sliding Window Median

---
+++ SlidingWindowMaximum/SlidingWindowMaximum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlidingWindowMaximum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //3,3,5,5,6,7
+            Console.WriteLine(string.Join(",", new Solution().MaxSlidingWindow(new []{ 1,3,-1,-3,5,3,6,7 }, 3)));
+            
+            //7, 4
+            Console.WriteLine(string.Join(",", new Solution().MaxSlidingWindow(new []{ 7, 2, 4 }, 2)));
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] MaxSlidingWindow(int[] nums, int k)
+    {
+        var result = new List<int>();
+        var queue = new List<int>();
+
+        var last = 0;
+        var first = 1 - k;
+        
+        for (; last < nums.Length; last++, first++)
+        {
+            while (queue.Count != 0 && nums[last] > queue[queue.Count - 1])
+            {
+                queue.RemoveAt(queue.Count - 1);
+            }
+            
+            queue.Add(nums[last]);
+
+            if (first < 0)
+            {
+                continue;
+            }
+
+            result.Add(queue[0]);
+
+            if (nums[first] == queue[0])
+            {
+                queue.RemoveAt(0);
+            }
+        }
+        
+        return result.ToArray();
+    }
+}
---
+++ SlidingWindowMedian/SlidingWindowMedian/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SlidingWindowMedian
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1, -1, -1, 3, 5, 6
+            Console.WriteLine(
+                string.Join(", ", new Solution().MedianSlidingWindow(new[] {1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+            //2147483647
+            Console.WriteLine(
+                string.Join(", ", new Solution().MedianSlidingWindow(new[] { 2147483647, 2147483647 }, 2)));
+        }
+    }
+}
+
+public class Solution
+{
+    public double[] MedianSlidingWindow(int[] nums, int k)
+    {
+        var N = nums.Length - k + 1;
+
+        if (N <= 0)
+        {
+            return new double[0];
+        }
+
+        var result = new double[N];
+        var sorted = new List<int>();
+
+        for (var i = 0; i < N; i++)
+        {
+            if (i == 0)
+            {
+                for (var j = i; j < k; j++)
+                {
+                    InsertElement(sorted, nums[j]);
+                }
+            }
+
+            if (i != 0)
+            {
+                sorted.Remove(nums[i - 1]);
+                InsertElement(sorted, nums[i + k - 1]);
+            }
+
+            result[i] = FindMedian(sorted, k);
+        }
+
+        return result;
+    }
+
+    private void InsertElement(List<int> list, int value)
+    {
+        var index = 0;
+
+        while (index < list.Count && value > list[index])
+        {
+            index++;
+        }
+
+        list.Insert(index, value);
+    }
+
+    private double FindMedian(List<int> sorted, int k)
+    {
+        if (k % 2 == 0)
+        {
+            return ((long)sorted.ElementAt(k / 2 - 1) + (long)sorted.ElementAt(k / 2)) / 2.0;
+        }
+
+        return sorted.ElementAt(k / 2);
+    }
+}
