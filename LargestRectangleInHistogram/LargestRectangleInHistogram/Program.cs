f738c6171daee25e28dc908e49c3647d9b9cb2fd
Timur Koshel <timurkoshel@gmail.com>
2020-05-31 21:14:41 +0300

    Larget rectangle in histogram and shortest subarray with sum at least K

---
+++ LargestRectangleInHistogram/LargestRectangleInHistogram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LargestRectangleInHistogram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(10 == new Solution().LargestRectangleArea(new[] {2, 1, 5, 6, 2, 3}));
+            Console.WriteLine(0 == new Solution().LargestRectangleArea(new int[0]));
+        }
+    }
+}
+
+public class Solution
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        if (heights.Length == 0)
+        {
+            return 0;
+        }
+        
+        var max = int.MinValue;
+        
+        for (var i = 0; i < heights.Length; i++)
+        {
+            var minElement = int.MaxValue;
+            
+            for (var j = i; j < heights.Length; j++)
+            {                
+                minElement = Math.Min(minElement, heights[j]);
+                max = Math.Max(minElement * (j + 1 - i), max);
+            }
+        }
+
+        return max;
+    }
+}
---
+++ ShortestSubarrayWithSumAtLeastK/ShortestSubarrayWithSumAtLeastK/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestSubarrayWithSumAtLeastK
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().ShortestSubarray(new[] {1}, 1));
+            Console.WriteLine(-1 == new Solution().ShortestSubarray(new[] {1, 2}, 4));
+            Console.WriteLine(3 == new Solution().ShortestSubarray(new[] {2, -1, 2}, 3));
+            Console.WriteLine(2 == new Solution().ShortestSubarray(new[] { 17,85,93,-45,-21 }, 150));
+        }
+    }
+}
+
+public class Solution
+{
+    public int ShortestSubarray(int[] A, int K)
+    {
+        var N = A.Length;
+        var p = new long[N + 1];
+
+        for (var i = 0; i < N; i++)
+        {
+            p[i + 1] = p[i] + A[i];
+        }
+
+        var result = N + 1;
+        var monoq = new LinkedList<int>();
+
+        for (var i = 0; i < p.Length; i++)
+        {
+            while (monoq.Count != 0 && p[i] <= p[monoq.Last.Value])
+            {
+                monoq.RemoveLast();
+            }
+
+            while (monoq.Count != 0 && p[i] >= p[monoq.First.Value] + K)
+            {
+                result = Math.Min(result, i - monoq.First.Value);
+                monoq.RemoveFirst();
+            }
+
+            monoq.AddLast(i);
+        }
+
+        return result < N + 1 ? result : -1;
+    }
+}
