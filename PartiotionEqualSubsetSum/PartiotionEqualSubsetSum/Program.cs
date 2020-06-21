741620166c756f00e5e0070e4f5a100fcd5bf257
Timur Koshel <timurkoshel@gmail.com>
2020-06-21 16:41:45 +0300

    Partition Equal Subset sum and Partition To K equal sum sumbsets

---
+++ PartiotionEqualSubsetSum/PartiotionEqualSubsetSum/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartiotionEqualSubsetSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().CanPartition(new int[] { 1, 5, 11, 5 }));
+            Console.WriteLine(false == new Solution().CanPartition(new int[] { 1, 2, 3, 5 }));
+            Console.WriteLine(false == new Solution().CanPartition(new int[] { 1, 2, 5 }));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool CanPartition(int[] nums)
+    {
+        var sum = 0;
+
+        foreach (var num in nums)    
+        {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1)
+        {
+            return false;
+        }
+
+        sum /= 2;
+
+        var n = nums.Length;
+        var dp = new bool[n + 1, sum + 1];
+            dp[0, 0] = true;
+
+        for (var i = 1; i <= n; i++)
+        {
+            dp[i, 0] = true;
+        }
+        
+        for (var j = 1; j <= sum; j++)
+        {
+            dp[0, j] = false;
+        }
+
+        for (var i = 1; i <= n; i++)
+        {
+            for (var j = 1; j <= sum; j++)
+            {
+                dp[i, j] = dp[i - 1, j];
+
+                if (j >= nums[i - 1])
+                {
+                    dp[i, j] = dp[i, j] || dp[i - 1, j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[n, sum];
+    }
+}
+
+public class SolutionSlow
+{
+    public bool CanPartition(int[] nums)
+    {
+        var memo = new Dictionary<string, bool>();
+        return CanPartitionHelper(nums, nums.Sum(), 0, 0, memo);
+    }
+
+    private bool CanPartitionHelper(int[] nums, int sumA, int sumB, int index, Dictionary<string, bool> memo)
+    {
+        if (index == nums.Length - 1)
+        {
+            return sumA == sumB;
+        }
+
+        var key = sumA + "_" + sumB;
+
+        if (memo.ContainsKey(key))
+        {
+            return memo[key];
+        }
+
+        var currentNum = nums[index];
+        index++;
+
+        memo[key] = CanPartitionHelper(nums, sumA - currentNum, sumB + currentNum, index, memo) ||
+                    CanPartitionHelper(nums, sumA, sumB, index, memo);
+        
+        return memo[key];
+    }
+}
---
+++ PartitionToKEqualSumSubsets/PartitionToKEqualSumSubsets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace PartitionToKEqualSumSubsets
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().CanPartitionKSubsets(new []{ 4, 3, 2, 3, 5, 2, 1 }, 4));
+            Console.WriteLine(false == new Solution().CanPartitionKSubsets(new []{ 4, 3, 2, 3, 5, 2 }, 4));
+            Console.WriteLine(true == new Solution().CanPartitionKSubsets(new []{ 10,10,10,7,7,7,7,7,7,6,6,6 }, 3));
+            
+        }
+    }
+}
+
+public class Solution
+{
+    public bool CanPartitionKSubsets(int[] nums, int k)
+    {
+        var sum = nums.Sum();
+        if (sum % k != 0)
+        {
+            return false;
+        }
+        
+        var targetSum = sum / k;
+        
+        return Helper(nums, new HashSet<int>(), 0, k, 0, 0, targetSum);
+    }
+
+    private bool Helper(int[] nums, HashSet<int> visited, int startIndex, int k, int currentSum, int currentNum, int target)
+    {
+        if (k == 1)
+        {
+            return true;
+        }
+
+        if (currentSum == target && currentNum > 0)
+        {
+            return Helper(nums, visited, 0, k - 1, 0, 0, target);
+        }
+
+        for (var i = startIndex; i < nums.Length; i++)
+        {
+            if (visited.Contains(i))
+            {
+                continue;
+            }
+
+            visited.Add(i);
+            if (Helper(nums, visited, i + 1, k, currentSum + nums[i], currentNum++, target))
+            {
+                return true;
+            }
+            visited.Remove(i);
+        }
+
+        return false;
+    }
+}
