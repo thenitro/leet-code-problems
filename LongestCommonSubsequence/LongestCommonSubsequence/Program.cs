1323572314761660541a689362ed0ad110b965a8
Timur Koshel <timurkoshel@gmail.com>
2020-06-23 19:11:43 +0300

    Longest Common Subsequence

---
+++ LongestCommonSubsequence/LongestCommonSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LongestCommonSubsequence
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(3 == new Solution().LongestCommonSubsequence("abcde", "ace"));
+            Console.WriteLine(3 == new Solution().LongestCommonSubsequence("abc", "abc"));
+            Console.WriteLine(0 == new Solution().LongestCommonSubsequence("abc", "def"));
+            Console.WriteLine(1 == new Solution().LongestCommonSubsequence("bsbininm", "jmjkbkjkv"));
+        }
+    }
+}
+
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        var dp = new int[text1.Length + 1, text2.Length + 1];
+
+        for (var i = 1; i <= text1.Length; i++)
+        {
+            for (var j = 1; j <= text2.Length; j++)
+            {
+                dp[i, j] = text1[i - 1] == text2[j - 1] ? dp[i - 1, j - 1] + 1 : Math.Max(dp[i - 1, j], dp[i, j - 1]);
+            }
+        }
+
+        return dp[text1.Length, text2.Length];
+    }
+}
---
+++ LongestCommonSubsequence/LongestCommonSubsequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LongestCommonSubsequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LongestCommonSubsequence")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("120DF5A7-0D33-4FAD-9C24-3352D0D2C851")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
