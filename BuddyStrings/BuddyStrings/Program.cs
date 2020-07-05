084ad5f3c4066594e4db309628526c817badc135
Timur Koshel <timurkoshel@gmail.com>
2020-07-05 20:40:27 +0300

    Buddy strings and Greatest common divisor of strings

---
+++ BuddyStrings/BuddyStrings/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BuddyStrings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(true == new Solution().BuddyStrings("ab", "ba"));
+            Console.WriteLine(false == new Solution().BuddyStrings("ab", "ab"));
+            Console.WriteLine(true == new Solution().BuddyStrings("aa", "aa"));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool BuddyStrings(string A, string B)
+    {
+        if (A.Length != B.Length)
+        {
+            return false;
+        }
+
+        if (A.Equals(B))
+        {
+            var s = new HashSet<char>();
+
+            foreach (var c in A)
+            {
+                if (!s.Contains(c))
+                {
+                    s.Add(c);                    
+                }
+            }
+
+            return s.Count < A.Length;
+        }
+        
+        
+        var diff = new List<int>();
+
+        for (var i = 0; i < A.Length; i++)
+        {
+            if (A[i] != B[i])
+            {
+                diff.Add(i);
+            }
+        }
+
+        return diff.Count == 2 && A[diff[0]] == B[diff[1]] && A[diff[1]] == B[diff[0]];
+    }
+}
---
+++ GreatesstCommonDivisorOfStrings/GreatesstCommonDivisorOfStrings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GreatesstCommonDivisorOfStrings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ABC" == new Solution().GcdOfStrings("ABCABC", "ABC"));
+            Console.WriteLine("AB" == new Solution().GcdOfStrings("ABABAB", "ABAB"));
+            Console.WriteLine("" == new Solution().GcdOfStrings("LEET", "CODE"));
+        }
+    }
+}
+
+public class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+        {
+            return string.Empty;
+        }
+
+        var len = str1.Length;
+        var len2 = str2.Length;
+
+        var gcd = Gcd(len, len2);
+        if (len < len2)
+        {
+            return str1.Substring(0, gcd);
+        }
+
+        return str2.Substring(0, gcd);
+    }
+
+    public int Gcd(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+
+        return a % b == 0 ? b : Gcd(b, a % b);
+    }
+}
