2222a36335ea304f03ea25600d033401747cb55a
Timur Koshel <timurkoshel@gmail.com>
2020-07-20 19:49:06 +0300

    Unique email address

---
+++ UniqueEmailAddresses/UniqueEmailAddresses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniqueEmailAddresses
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().NumUniqueEmails(new [] { "test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com" }));
+        }
+    }
+}
+
+public class Solution
+{
+    public int NumUniqueEmails(string[] emails)
+    {
+        var result = new HashSet<string>();
+
+        foreach (var email in emails)
+        {
+            var splitted = email.Split('@');
+            var left = splitted[0];
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < left.Length; i++)
+            {
+                if (left[i] == '+')
+                {
+                    break;
+                }
+                
+                if (left[i] == '.')
+                {
+                    continue;
+                } 
+                
+                sb.Append(left[i]);
+            }
+
+            sb.Append('@');
+            sb.Append(splitted[1]);
+
+            result.Add(sb.ToString());
+        }
+        
+        return result.Count;
+    }
+}
