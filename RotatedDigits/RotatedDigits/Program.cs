74955405dc78fda66ec0e2f0377483b6da41da0e
Timur Koshel <timurkoshel@gmail.com>
2020-07-20 19:38:58 +0300

    Rotated digits

---
+++ RotatedDigits/RotatedDigits/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RotatedDigits
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(4 == new Solution().RotatedDigits(10));
+            Console.WriteLine(1 == new Solution().RotatedDigits(2));
+            Console.WriteLine(247 == new Solution().RotatedDigits(857));
+        }
+    }
+}
+
+public class Solution
+{
+    public int RotatedDigits(int N)
+    {
+        var count = 0;
+
+        for (var i = 1; i <= N; i++)
+        {
+            if (IsGoodNumber(i))
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+
+    private bool IsGoodNumber(int number)
+    {
+        var validFound = false;
+
+        while (number > 0)
+        {
+            var digit = number % 10;
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
+            {
+                validFound = true;
+            }
+
+            if (digit == 3 || digit == 4 || digit == 7)
+            {
+                return false;
+            }
+
+            number = number / 10;
+        }
+        
+        return validFound;
+    }
+}
