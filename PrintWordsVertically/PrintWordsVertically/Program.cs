456d49fc289118cbd28c1f767c9459e7f853cb15
Timur Koshel <timurkoshel@gmail.com>
2020-07-24 16:40:28 +0300

    Print words vertically

---
+++ PrintWordsVertically/PrintWordsVertically/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintWordsVertically
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", new Solution().PrintVertically("HOW ARE YOU")));
+            Console.WriteLine("*" + string.Join(",", new Solution().PrintVertically("TO BE OR NOT TO BE")) + "*");
+            Console.WriteLine(string.Join(",", new Solution().PrintVertically("CONTEST IS COMING")));
+            Console.WriteLine(string.Join(",", new Solution().PrintVertically("AA BBB C DDDD EEEEE F")));
+        }
+    }
+}
+
+public class Solution
+{
+    public IList<string> PrintVertically(string s)
+    {
+        var splitted = s.Split(' ');
+        var longestWord = GetLongestWord(splitted);
+
+        var result = new string[longestWord];
+
+        for (var j = 0; j < longestWord; j++)
+        {
+            for (var i = 0; i < splitted.Length; i++)
+            {
+                if (i == 0)
+                {
+                    result[j] = "";
+                }
+
+                var word = splitted[i];                
+                var append = j >= word.Length ? ' ' : word[j];
+
+                result[j] += append;
+            }
+        }
+
+        for (var j = 0; j < result.Length; j++)
+        {
+            var word = result[j];
+            var splitLength = word.Length;
+        
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                if (word[i] != ' ')
+                {
+                    splitLength = i + 1;
+                    break;
+                }
+            }
+            
+            result[j] = word.Substring(0, splitLength);
+        }
+        
+        return result;
+    }
+
+    private int GetLongestWord(string[] splitted)
+    {
+        var max = int.MinValue;
+
+        foreach (var s in splitted)
+        {
+            max = Math.Max(s.Length, max);
+        }
+
+        return max;
+    }
+}
---
+++ PrintWordsVertically/PrintWordsVertically/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrintWordsVertically")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrintWordsVertically")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("991A0999-615C-4B15-8950-1CD5C9C644B6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
