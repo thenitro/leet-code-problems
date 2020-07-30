ac053f7f29ec3d7ba6306acc848c0642d08914cb
Timur Koshel <timurkoshel@gmail.com>
2020-07-30 14:27:18 +0300

    Wildcard Matching

---
+++ WildcardMatching/WildcardMatching/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WildcardMatching
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(false == new Solution().IsMatch("aa", "a"));
+            //Console.WriteLine(true == new Solution().IsMatch("aa", "??"));
+            //Console.WriteLine(true == new Solution().IsMatch("aa", "*"));
+            //Console.WriteLine(false == new Solution().IsMatch("cb", "?a"));
+            Console.WriteLine(true == new Solution().IsMatch("adceb", "*a*b"));
+            //Console.WriteLine(false == new Solution().IsMatch("acdcb", "a*c?b"));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool IsMatch(string s, string p)
+    {
+        var indexS = 0;
+        var indexP = 0;
+        var match = 0;
+        var starIdx = -1;
+
+        while (indexS < s.Length)
+        {
+            if (indexP < p.Length && (p[indexP] == '?' || s[indexS] == p[indexP]))
+            {
+                indexP++;
+                indexS++;
+            }
+            else if (indexP < p.Length && p[indexP] == '*')
+            {
+                starIdx = indexP;
+                match = indexS;
+                indexP++;
+            }
+            else if (starIdx != -1)
+            {
+                indexP = starIdx + 1;
+                match++;
+                indexS = match;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        while (indexP < p.Length && p[indexP] == '*')
+        {
+            indexP++;
+        };
+        
+        return indexP == p.Length;
+    }
+}
