c878cbc169d43bc2f17451afebc8e10a91ae948f
Tymur Koshel <timurkoshel@gmail.com>
2021-05-21 19:52:26 +0300

    50. Pow(x, n)

---
+++ 2021/Pow/Pow/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 == new Solution().MyPow(12, 0));
+            Console.WriteLine(12 == new Solution().MyPow(12, 1));
+            Console.WriteLine(12 * 12 == new Solution().MyPow(12, 2));
+            Console.WriteLine(0.25 == new Solution().MyPow(2, -2));
+        }
+    }
+    
+    public class Solution {
+        public double MyPow(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n < 0)
+            {
+                if (n == int.MinValue)
+                {
+                    n /= 2;
+                    x *= x;
+                }
+
+                n = -n;
+                x = 1 / x;
+            }
+
+            if (n % 2 == 0)
+            {
+                return MyPow(x * x, n / 2);
+            }
+
+            return MyPow(x * x, n / 2) * x;
+        }
+    }
+}
---
+++ 2021/Pow/Pow/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pow")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F8F44B32-57A1-4ECC-B194-A11F22D90B6F")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
