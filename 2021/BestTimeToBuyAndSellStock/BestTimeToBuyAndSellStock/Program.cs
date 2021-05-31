e631c34123bac60420739c80609e2050d9c90e5b
Tymur Koshel <timurkoshel@gmail.com>
2021-05-31 07:43:29 +0300

    121. Best time to buy and sell stock

---
+++ 2021/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(5 == new Solution().MaxProfit(new[] {7, 1, 5, 3, 6, 4}));
+            Console.WriteLine(0 == new Solution().MaxProfit(new[] {7, 6, 4, 3, 1}));
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var min = int.MaxValue;
+            var maxDiff = 0;
+
+            for (var i = 0; i < prices.Length; i++)
+            {
+                min = Math.Min(min, prices[i]);
+                var diff = prices[i] - min;
+
+                maxDiff = Math.Max(maxDiff, diff);
+            }
+
+            return maxDiff;
+        }
+    }
+}
