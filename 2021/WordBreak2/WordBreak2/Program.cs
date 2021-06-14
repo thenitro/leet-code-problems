0a188d553b777da2762e1209f162a4d9cee91b12
Tymur Koshel <timurkoshel@gmail.com>
2021-06-14 11:00:45 +0300

    140. Word Break II

---
+++ 2021/WordBreak2/WordBreak2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordBreak2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",",
+                new Solution().WordBreak("catsanddog", new List<string>() {"cat", "cats", "and", "sand", "dog"})));
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            var result = new List<string>();
+            Helper(s, wordDict, string.Empty, result);
+            return result;
+        }
+
+        private void Helper(string s, IList<string> dict, string currentResult, List<string> result)
+        {
+            if (s.Length == 0)
+            {
+                result.Add(currentResult);
+                return;
+            }
+
+            foreach (var word in dict)
+            {
+                if (s.StartsWith(word))
+                {
+                    Helper(s.Substring(word.Length), dict, string.IsNullOrEmpty(currentResult) ? word : currentResult + " " + word, result);
+                }
+            }
+        }
+    }
+}
