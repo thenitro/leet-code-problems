f3abc20dbe36db4ba5367274f7ecebb35c0aa0c2
Tymur Koshel <tymurkoshel@apples-MacBook-Pro.local>
2021-07-05 12:44:06 +0300

    238. Product of Array Except Self

---
+++ 2021/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", new Solution().ProductExceptSelf(new []{ 1, 2, 3, 4 })));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var N = nums.Length;
+
+            var leftProduct = new int[N];
+                leftProduct[0] = 1;
+            
+            var rightProduct = new int[N];
+                rightProduct[N - 1] = 1;
+
+            for (var i = 1; i < N; i++)
+            {
+                leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+            }
+
+            for (var i = N - 2; i >= 0; i--)
+            {
+                rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+            }
+
+            var result = new int[N];
+
+            for (var i = 0; i < N; i++)
+            {
+                result[i] = leftProduct[i] * rightProduct[i];
+            }
+
+            return result;
+        }
+    }
+}
