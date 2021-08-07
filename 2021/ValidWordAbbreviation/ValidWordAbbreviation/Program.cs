526bc3f4f236831360f20710c3b1321bcf12bb5c
Tymur Koshel <timurkoshel@gmail.com>
2021-08-07 10:15:12 +0300

    Valid Word Abbreviation

---
+++ 2021/ValidWordAbbreviation/ValidWordAbbreviation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ValidWordAbbreviation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().ValidWordAbbreviation("internationalization", "i12iz4n"));
+            Console.WriteLine(false == new Solution().ValidWordAbbreviation("apple", "a2e"));
+            Console.WriteLine(new Solution().ValidWordAbbreviation("internationalization", "i5a11o1"));
+        }
+    }
+}
+
+public class Solution
+{
+    public bool ValidWordAbbreviation(string word, string abbr)
+    {
+        var wordIndex = 0;
+        var abbrIndex = 0;
+
+        while (wordIndex < word.Length && abbrIndex < abbr.Length)
+        {
+            if (char.IsDigit(abbr[abbrIndex]))
+            {
+                if (abbr[abbrIndex] == '0')
+                {
+                    return false;
+                }
+
+                var number = 0;
+
+                while (abbrIndex < abbr.Length && char.IsDigit(abbr[abbrIndex]))
+                {
+                    number = number * 10 + (abbr[abbrIndex] - '0');
+                    abbrIndex++;
+                }
+
+                wordIndex += number;
+            }
+            else
+            {
+                if (word[wordIndex] != abbr[abbrIndex])
+                {
+                    return false;
+                }
+                else
+                {
+                    wordIndex++;
+                    abbrIndex++;
+                }
+            }
+        }
+        
+        return wordIndex == word.Length && abbrIndex == abbr.Length;
+    }
+}
