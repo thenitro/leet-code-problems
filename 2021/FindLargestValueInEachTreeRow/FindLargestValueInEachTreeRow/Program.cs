375d628c2e161ec75d830e2a4d843889f5b9ee18
Tymur Koshel <timurkoshel@gmail.com>
2021-08-07 12:59:01 +0300

    515. Find Largest Value in Each Tree Row

---
+++ 2021/FindLargestValueInEachTreeRow/FindLargestValueInEachTreeRow/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindLargestValueInEachTreeRow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> LargestValues(TreeNode root)
+    {
+        var results = new List<int>();
+
+        if (root == null)
+        {
+            return results;
+        }
+
+        var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+        var count = 1;
+        var currentMax = int.MinValue;
+            
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            count--;
+
+            currentMax = Math.Max(node.val, currentMax);
+
+            if (node.left != null)
+            {
+                queue.Enqueue(node.left);
+            }
+
+            if (node.right != null)
+            {
+                queue.Enqueue(node.right);
+            }
+
+            if (count == 0)
+            {
+                results.Add(currentMax);
+                currentMax = int.MinValue;
+                count = queue.Count;
+            }
+        }
+        
+        return results;
+    }
+}
