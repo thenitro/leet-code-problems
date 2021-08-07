73c5d82ee1d74d32a47970b1a74a367f31e785d3
Tymur Koshel <timurkoshel@gmail.com>
2021-08-07 16:26:13 +0300

    621. Task Scheduler

---
+++ 2021/TaskScheduler/TaskScheduler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskScheduler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(8 == new Solution().LeastInterval(new char[] { 'A','A','A','B','B','B' }, 2));
+            Console.WriteLine(16 == new Solution().LeastInterval(new char[] { 'A','A','A','A','A','A','B','C','D','E','F','G' }, 2));
+        }
+    }
+}
+
+public class Solution
+{
+    public int LeastInterval(char[] tasks, int n)
+    {
+        if (n == 0)
+        {
+            return tasks.Length;
+        }
+
+        var count = new int[26];
+        var max = 0;
+        var longest = 0;
+
+        foreach (var task in tasks)
+        {
+            count[task - 'A']++;
+            max = Math.Max(max, count[task - 'A']);
+        }
+
+        foreach (var i in count)
+        {
+            if (i == max)
+            {
+                longest++;
+            }
+        }
+
+        var total = (max - 1) * (n + 1) + longest;
+        return total > tasks.Length ? total : tasks.Length;
+    }
+}
