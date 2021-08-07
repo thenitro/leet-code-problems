a9bdea1b0e3b248b31f3f415169b2d32d3256ba0
Tymur Koshel <timurkoshel@gmail.com>
2021-08-07 13:15:24 +0300

    560. Subarray Sum Equals K

---
+++ 2021/SubarraySum/SubarraySum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SubarraySum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 == new Solution().SubarraySum(new int[] { 1,1,1}, 2));
+            Console.WriteLine(2 == new Solution().SubarraySum(new int[] { 1,2,3}, 3));
+            Console.WriteLine(3 == new Solution().SubarraySum(new int[] { 1,-1,0}, 0));
+        }
+    }
+}
+
+public class Solution
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        var amount = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var sum = 0;
+
+            for (var j = i; j < nums.Length; j++)
+            {
+                sum += nums[j];
+
+                if (sum == k)
+                {
+                    amount++;
+                }
+            }
+        }
+
+        return amount;
+    }
+}
