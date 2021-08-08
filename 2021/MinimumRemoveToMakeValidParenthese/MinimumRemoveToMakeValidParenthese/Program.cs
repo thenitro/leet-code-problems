cccf5d934fe740493502c1213d73e3d4a897ce35
Tymur Koshel <timurkoshel@gmail.com>
2021-08-08 08:32:09 +0300

    1249. Minimum Remove to Make Valid Parentheses

---
+++ 2021/MinimumRemoveToMakeValidParenthese/MinimumRemoveToMakeValidParenthese/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinimumRemoveToMakeValidParenthese
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("lee(t(c)o)de" == new Solution().MinRemoveToMakeValid("lee(t(c)o)de)"));
+        }
+    }
+}
+
+public class Solution
+{
+    public string MinRemoveToMakeValid(string s)
+    {
+        var indexesToRemove = new HashSet<int>();
+        var stack = new Stack<int>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(')
+            {
+                stack.Push(i);
+            }
+
+            if (s[i] == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    indexesToRemove.Add(i);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+        }
+
+        while (stack.Count > 0)
+        {
+            indexesToRemove.Add(stack.Pop());
+        }
+
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (!indexesToRemove.Contains(i))
+            {
+                sb.Append(s[i]);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
