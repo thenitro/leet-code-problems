fb84fee46a89b9e093968b9e149796e700949d66
Tymur Koshel <timurkoshel@gmail.com>
2021-08-09 08:02:18 +0300

    1650. Lowest Common Ancestor of a Binary Tree III

---
+++ 2021/LowestCommonAncestorOfBinaryTree3/LowestCommonAncestorOfBinaryTree3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LowestCommonAncestorOfBinaryTree3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public class Solution
+{
+    public Node LowestCommonAncestor(Node p, Node q)
+    {
+        var set = new HashSet<Node>();
+        
+        while (p != null)
+        {
+            set.Add(p);
+            p = p.parent;
+        }
+        
+        while (q != null)
+        {
+            if (set.Contains(q))
+            {
+                return q;
+            }
+
+            set.Add(q);
+            q = q.parent;
+        }
+
+        return null;
+    }
+}
+
+public class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
