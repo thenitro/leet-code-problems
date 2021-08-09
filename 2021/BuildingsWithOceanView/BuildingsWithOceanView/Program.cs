03439bed6ff37015d911723270def01db8b3dbb6
Tymur Koshel <timurkoshel@gmail.com>
2021-08-09 14:29:29 +0300

    1762. Buildings With an Ocean View

---
+++ 2021/BuildingsWithOceanView/BuildingsWithOceanView/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace BuildingsWithOceanView
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] FindBuildings(int[] heights)
+    {
+        var stack = new Stack<int>();
+
+        for (var i = 0; i < heights.Length; i++)
+        {
+            var currentHeight = heights[i];
+
+            while (stack.Count > 0 && heights[stack.Peek()] <= currentHeight)
+            {
+                stack.Pop();
+            }
+            
+            stack.Push(i);
+        }
+
+        var result = new int[stack.Count];
+
+        while (stack.Count > 0)
+        {
+            result[stack.Count - 1] = stack.Pop();
+        }
+
+        return result;
+    }
+}
