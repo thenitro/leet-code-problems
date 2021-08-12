463175046714191da24cb884cf43a91d53c14960
Tymur Koshel <timurkoshel@gmail.com>
2021-08-12 11:12:51 +0300

    1047. Remove All Adjacent Duplicates In String

---
+++ 2021/RemoveAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveAllAdjacentDuplicatesInString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ca" == new Solution().RemoveDuplicates("abbaca"));
+            Console.WriteLine("ay" == new Solution().RemoveDuplicates("azxxzy"));
+        }
+    }
+}
+
+public class Solution
+{
+    public string RemoveDuplicates(string s)
+    {
+        var stack = new Stack<char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (stack.Count > 0 && stack.Peek() == s[i])
+            {
+                while (stack.Count > 0 && stack.Peek() == s[i])
+                {
+                    stack.Pop();
+                }
+            }
+            else
+            {
+                stack.Push(s[i]);
+            }
+        }
+
+        return new string(stack.Reverse().ToArray());
+    }
+}
