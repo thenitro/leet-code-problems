4450a1be2047598351fb21c88e661fb505258b16
Tymur Koshel <timurkoshel@gmail.com>
2021-08-12 06:02:07 +0300

    921. Minimum Add to Make Parentheses Valid

---
+++ 2021/MinimumAddToMakeParentheseValid/MinimumAddToMakeParentheseValid/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace MinimumAddToMakeParentheseValid
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public class Solution
+{
+    public int MinAddToMakeValid(string s)
+    {
+        var stack = new Stack<char>();
+        var closingParentheses = 0;
+
+        foreach (var c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    closingParentheses++;
+                }
+            }
+        }
+
+        return stack.Count + closingParentheses;
+    }
+}
