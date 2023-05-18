e59758586c90471e77cb3fd3b951af1d1e48a221
Tymur Koshel <timurkoshel@gmail.com>
2023-05-18 20:34:09 +0200

    Design Parking System

---
+++ 2023/ParkingSystem/ParkingSystem/Solution.cs
@@ -0,0 +1,62 @@
+public class ParkingSystem
+{
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public bool AddCar(int carType) {
+        if (carType == 1)
+        {
+            if (big > 0)
+            {
+                big--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (carType == 2)
+        {
+            if (medium > 0)
+            {
+                medium--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        if (carType == 3)
+        {
+            if (small > 0)
+            {
+                small--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
